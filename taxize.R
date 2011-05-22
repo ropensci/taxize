240fc90192c7277d50379c0710af272ddacbfd7b
Scott Chamberlain <myrmecocystus@gmail.com>
2011-05-22 10:24:01 -0500

    altered taxize.R

--- taxize.R
+++ taxize.R
@@ -75,6 +75,141 @@ match_itsnames(spnames, "sciname")
 
 
 
+# Functions to get trees from Phylomatic
+  # GET format
+    # xml format
+url <- "http://phylodiversity.net/phylomatic/pm/phylomatic.cgi?format=xml&tree=annonaceae%2Fannona%2Fannona_cherimola%0D%0Aannonaceae%2Fannona%2Fannona_muricata"
+tt <- getURLContent(url, curl=getCurlHandle())
+page <- xmlParse(tt)
+page
+page_ <- xmlToList(page)
+page_$newick
+    # newick format
+url <- "http://phylodiversity.net/phylomatic/pm/phylomatic.cgi?format=new&tree=annonaceae%2Fannona%2Fannona_cherimola%0D%0Aannonaceae%2Fannona%2Fannona_muricata"
+tt <- getURLContent(url, curl=getCurlHandle())
+tt[[1]]
+
+
+  # POST format
+    # xml format
+url <- "http://phylodiversity.net/phylomatic/pm/phylomatic.cgi"
+x <- postForm(url,
+  format = 'xml', 
+  tree = 'annonaceae%2Fannona%2Fannona_cherimola%0D%0Aannonaceae%2Fannona%2Fannona_muricata'
+  )
+x[[1]]
+
+    # newick format
+url <- "http://phylodiversity.net/phylomatic/pm/phylomatic.cgi"
+x <- postForm(url,
+  format = 'new', 
+  tree = 'annonaceae%2Fannona%2Fannona_cherimola%0D%0Aannonaceae%2Fannona%2Fannona_muricata'
+  )
+x[[1]]
+
+
+# Format tree string, submit to Phylomatic, get newick tree
+# Submitted in POST format (not GET format)
+# Version: already have in Phylomatic input format
+# forward slash (/ -> %2F)
+# newline (\n -> %0D%0A)
+# output: newick tree
+# input: x = phylomatic format input, format = newick or xml output, 
+# retphylo = return phylo tree object (TRUE or FALSE)
+x <- phyformat
+get_phylomatic_tree <- function (x, get, format, retphylo = TRUE) {
+  url <- "http://phylodiversity.net/phylomatic/pm/phylomatic.cgi"
+  treestring <- str_replace_all(str_replace_all(x, "/", "%2F"), "\n", "%0D%0A")
+  
+  collapse_double_root <- function(y) {
+    temp <- str_split(y, ")")[[1]]
+    double <- c(length(temp)-1, length(temp))
+    tempsplit <- temp[double]
+    tempsplit_1 <- str_split(tempsplit[1], ":")[[1]][2]
+    tempsplit_2 <-str_split(tempsplit[2], ":")[[1]]
+    rootlength <- as.numeric(tempsplit_1) + 
+      as.numeric(str_split(tempsplit_2[2], ";")[[1]][1])
+    newx <- paste(")", tempsplit_2[1], ":", rootlength, ";", sep="")
+    newpre <- str_replace(temp[1], "[(]", "")
+    allelse <- temp[-1]
+    allelse <- allelse[setdiff(1:length(allelse), double-1)]
+    allelse <- paste(")", allelse, sep="")
+    tempdone <- paste(newpre, paste(allelse, collapse=""), newx, sep="")
+  return(tempdone)
+  }
+
+  colldouble <- function(z) {
+    if ( class ( try ( read.tree(text = z), silent = T ) ) %in% 'try-error') 
+      { treephylo <- collapse_double_root(z) } else
+      { treephylo <- z }
+  return(treephylo)
+  }
+  
+  if (get == 'POST') {  
+    gettree <- postForm(url,
+        format = format, 
+        tree = treestring
+        )
+    tree_ <- gsub("\n", "", gettree[[1]]) 
+    treenew <- colldouble(tree_)
+  } else
+  
+  if (get == 'GET') {0
+    if (format == 'xml') {
+      urlplus <- paste(url, "?", "format=", format, "&tree=", treestring, sep="")
+      tt <- getURLContent(urlplus, curl=getCurlHandle())
+      page <- xmlParse(tt)
+      tree_ <- xmlToList(page)$newick
+      treenew <- colldouble(tree_)
+    } else
+    
+    if (format == 'new') {
+      urlplus <- paste(url, "?", "format=", format, "&tree=", treestring, sep="")
+      tt <- getURLContent(urlplus, curl=getCurlHandle())
+      tree <- tt[[1]]
+      tree_ <- gsub("\n", "", tree[[1]])
+      treenew <- colldouble(tree_)
+    } else
+    {stop("Error: format must be one of 'xml' or 'new' for newick")}
+  } else
+  {stop("Error: get must be one of 'POST' or 'GET'")}
+  
+  if (retphylo == 'TRUE') {
+    treenew <- read.tree(text = treenew)
+  } else
+  { treenew <- treenew }
+return(treenew)
+}
+
+# E.g.
+phyformat <- "annonaceae/annona/annona_cherimola
+annonaceae/annona/annona_muricata"
+
+phyformat <- "annonaceae/annona/annona_cherimola
+annonaceae/annona/annona_muricata
+fagaceae/quercus/quercus_robur
+dipterocarpaceae/shorea/shorea_parvifolia
+pandanaceae/pandanus/pandanus_minor
+poaceae/oryza/oryza_sativa
+malvaceae/durio/durio_zibethinus
+rosaceae/rubus/rubus_ulmifolius
+apocynaceae/asclepias/asclepias_curassavia
+anacardiaceae/pistacia/pistacia_lentiscus"
+
+tree <- 
+get_phylomatic_tree(phyformat, 'GET', 'xml', 'TRUE')
+get_phylomatic_tree(phyformat, 'GET', 'new', 'FALSE')
+get_phylomatic_tree(phyformat, 'POST', 'xml', 'TRUE')
+tree <- get_phylomatic_tree(phyformat, 'POST', 'new', 'FALSE')
+treephylo <- read.tree(text = tree)
+
+
+
+
+
+
+
+
 
 
 
