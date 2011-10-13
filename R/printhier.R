8cb048933632dc9e6b818f83c37d4a9b871663bc
Scott Chamberlain <myrmecocystus@gmail.com>
2011-10-13 08:48:36 -0500

    edited and improved printhier functino

--- R/printhier.R
+++ R/printhier.R
@@ -1,35 +1,14 @@
+# printhier.R
+
+printhier <- function(hierout = NA) {
 # Function to print taxnomic hierarchy
-printhier <- function(searchterm, searchtype, by_, curl=getCurlHandle()) {
-  base_url <- "http://www.itis.gov/ITISWebService/services/ITISService/"
-  skey_ <- "srchKey="
-  tkey_ <- "tsn="
-  sciname_url <- "searchByScientificName?"
-  anymatch_url <- "searchForAnyMatch?"
-  comnamebeg_url <- "searchByCommonNameBeginsWith?"
-  comname_url <- "searchByCommonName?"
-  comnameend_url <- "searchByCommonNameEndsWith?"
-  itisterms_url <- "getITISTerms?"
-  itistermscomname_url <- "getITISTermsFromCommonName?"
-  itistermssciname_url <- "getITISTermsFromScientificName?"
-  tsnsvernacular_url <- "getTsnByVernacularLanguage?"
-  tsnfullhir_url <- "getFullHierarchyFromTSN?"
-  if(searchtype == "sciname") {bykey <- sciname_url} else
-  if(searchtype == "anymatch") {bykey <- anymatch_url} else
-  if(searchtype == "comnamebeg") {bykey <- comnamebeg_url} else
-  if(searchtype == "comname") {bykey <- comname_url} else
-  if(searchtype == "comnameend") {bykey <- comnameend_url} else
-  if(searchtype == "terms") {bykey <- itisterms_url} else
-  if(searchtype == "itistermscomname") {bykey <- itistermscomname_url} else
-  if(searchtype == "itistermssciname") {bykey <- itistermssciname_url} else
-  if(searchtype == "tsnsvernacular") {bykey <- tsnsvernacular_url} else
-  if(searchtype == "tsnfullhir") {bykey <- tsnfullhir_url} else
-    end
-  if (by_ ==  'name') { searchurl <- paste(base_url, bykey, skey_, searchterm, sep="") } else
-    if (by_ == 'tsn' ) { searchurl <- paste(base_url, bykey, tkey_, searchterm, sep="")  } 
-      end
-  tt <- getURLContent(searchurl, curl=curl)
-  page <- xmlTreeParse(tt)
+# Args:
+#   hierout: output from a hierarchy search using get_itis_xml function (list)
+# Output: 
+# Examples:
+#   out <- get_itis_xml("36616", "tsnfullhir", "tsn", FALSE)  
+#   printhier(out)  
+  page <- xmlTreeParse(hierout)
   templist <- ldply(xmlToList(page), function(x) data.frame(c(x[3], x[4])))[,-3]
-  hier <- na.omit(templist)  
-  return(hier)
+  na.omit(templist)
 }
