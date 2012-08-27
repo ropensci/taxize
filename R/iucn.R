5872920f48d400edaf22f406d3ef7a20b3394b2e
edisz <szoe8822@uni-landau.de>
2012-08-27 19:25:05 +0200

    added documentation for iucn_summary()

--- R/iucn.R
+++ R/iucn.R
@@ -1,3 +1,24 @@
+#' Get a summary from the IUCN Redlist
+#' 
+#' @import XML
+#' @param x scientific name 
+#' @return A list with the following items:
+#' \item{status}{Red List Category}
+#' \item{history}{History of status}
+#' \item{distr}{Geographic distribution}
+#' \item{pop}{Population size estimates}
+#' \item{trend}{Trend of population size}
+#' \export
+#' 
+#' @examples \dontrun{
+#' ia <- iucn_summary("Panthera uncia")
+#' ia$status
+#' ia$history
+#' ia$distr
+#' ia$pop
+#' ia$trend
+#' }
+
 iucn_summary <- function(x) {
   spec <- tolower(x)
   spec <- gsub(" ", "-", spec)
@@ -7,16 +28,16 @@ iucn_summary <- function(x) {
   history <- data.frame(year = xpathSApply(h, '//div[@class="year"]', xmlValue),
                         category = xpathSApply(h, '//div[@class="category"]', xmlValue))
   distr <- xpathSApply(h, '//ul[@class="countries"]', xmlValue)
-  distr <- unlist(strsplit(distr1, "\n"))
+  distr <- unlist(strsplit(distr, "\n"))
   pop <- xpathSApply(h, '//div[@id="population"]/text()[preceding-sibling::br]', xmlValue)
   trend <- xpathSApply(h, '//div[@id="population_trend"]', xmlValue)
   out <- list(status = status, 
-              histroy = history, 
+              history = history, 
               distr = distr, 
               pop = pop, 
               trend = trend)
   return(out)
 }
 
-#ia <- iucn_summary("Panthera uncia")
+
 
