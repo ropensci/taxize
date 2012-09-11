4b825386c0b9eed30680fd12c32eb562410c5892
EDiLD <szoe8822@uni-landau.de>
2012-09-11 12:54:32 +0200

    get_tsn() asks for user input, if > 1 TSNs are found for species.

--- R/get_tsn.R
+++ R/get_tsn.R
@@ -17,6 +17,9 @@
 #' get_tsn(searchterm="polar bear", searchtype="comname")
 #' get_tsn(c("Chironomus riparius","Quercus douglasii"), "sciname")
 #' get_tsn(c("aa aa", "Chironomus riparius"), searchtype="sciname")
+#' splist <- c("annona cherimola", 'annona muricata', "quercus robur", 
+#' "shorea robusta", "pandanus patina", "oryza sativa", "durio zibethinus")
+#' get_tsn(splist,"sciname")
 #' }
 get_tsn <- function (searchterm, searchtype) 
 {
@@ -34,9 +37,19 @@ get_tsn <- function (searchterm, searchtype)
     # should return NA if spec not found
     if (length(tsn) == 0)
       tsn <- NA
-    if (length(tsn) > 1)
-      warning("More then one TSN found for species '", x, "'. \nFirst TSN used!")
-      tsn <- tsn[1]
+    if (length(tsn) > 1){
+      cat("\n
+          More than one TSN found for species '", x, "'!\n
+          Enter rowname of species to take:\n") # prompt
+      print(tsn_df)
+      take <- scan(n = 1, quiet = TRUE)
+      if(take %in% seq_along(nrow(tsn_df))){
+        cat("Input accepted, took species '", as.character(tsn_df$combinedname[take]), "'\n")
+      } else {
+        stop("Non valid input!")
+      }
+      tsn <- tsn[take]
+    }
     tsn
   }
   out <- laply(searchterm, fun)
