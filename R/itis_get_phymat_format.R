78d90ee7f6de86a2472a792ff51f96fd39eb4bd4
EDiLD <szoe8822@uni-landau.de>
2012-10-14 21:41:24 +0200

    updated docs to recent changes

--- R/itis_get_phymat_format.R
+++ R/itis_get_phymat_format.R
@@ -1,10 +1,10 @@
 #' Get family names to make Phylomatic input object, and output input string 
-#'    to Phylomatic for use in the function get_phylomatic_tree.
+#'    to Phylomatic for use in the function phylomatic_tree.
 #'    
 #' @import XML RCurl stringr plyr
 #' @param tsn quoted tsn number (taxonomic serial number)
 #' @param format output format, isubmit (you can paste in to the Phylomatic 
-#     website), or 'rsubmit' to use in fxn get_phylomatic_tree
+#     website), or 'rsubmit' to use in fxn phylomatic_tree
 #' @return e.g., "pinaceae/pinus/pinus_contorta", in Phylomatic submission format.
 #' @examples \dontrun{
 #' dat_ <- laply(list("36616", "19322", "183327"), itis_get_phymat_format, 
