d19461b62a24c6f31c357134c42b300599b0ea23
Scott Chamberlain <myrmecocystus@gmail.com>
2012-10-16 10:30:48 -0700

    fixed downstream2 should work well now for all cases

--- R/downstream2.R
+++ R/downstream2.R
@@ -1,55 +1,54 @@
 #' Retrieve all taxa names or TSNs downstream in hierarchy from given TSN.
 #' 
-#' THE FUNCTION NEEDS TO BE GENERALIZED TO ANY RANK DESIRED IN THE OUTPUT
-#' 		JUST WORKS FOR GOING DOWN TO FAMILY RIGHT NOW.
-#' 
 #' @import XML RCurl ritis plyr
 #' @param tsns A taxonomic serial number. 
+#' @param downto The taxonomic level you want to go down to. See examples below.
+#' 		The taxonomic level is not case sensitive but you do have to spell it 
+#' 		correctly. See \code{data(rank_ref)} for spelling.
 #' @return Data.frame of taxonomic information downstream to family from e.g., 
 #' 		Order, Class, etc. 
 #' @author Scott Chamberlain {myrmecocystus@@gmail.com}
 #' @examples \dontrun{
-#' downstream2(tsns = 650497) # getting families downstream from Acridoidea
-#' downstream2(tsns = 114989) # getting families downstream from Strepsiptera
-#' downstream2(tsns = 99208) # getting families downstream from Class Insecta
+#' downstream2(846509, "Genus")
+#' downstream2(tsns = 650497, "Family") # getting families downstream from Acridoidea
+#' downstream2(tsns = 180541, "Species") # getting species downstream from Ursus
 #' }
 #' @export
-downstream2 <- function(tsns) {
-	lets <- c(LETTERS, paste(LETTERS, LETTERS, sep=""), 
-						paste(LETTERS, LETTERS, LETTERS, sep=""), 
-						paste(LETTERS, LETTERS, LETTERS, LETTERS, sep=""), 
-						paste(LETTERS, LETTERS, LETTERS, LETTERS, LETTERS, sep=""),
-						paste(LETTERS, LETTERS, LETTERS, LETTERS, LETTERS, LETTERS, sep=""),
-						paste(LETTERS, LETTERS, LETTERS, LETTERS, LETTERS, LETTERS, LETTERS, sep=""))
-	fam <- "not" 
+downstream2 <- function(tsns, downto) 
+{
+	# load rank reference data.frame
+	if(exists(as.character(substitute(rank_ref)))==TRUE){ NULL } else
+		{ data(rank_ref); message("loaded rank_ref") }
+	downto2 <- rank_ref[grep(downto, rank_ref$ranks, ignore.case=T),"rankId"]
+	torank_ids <- rank_ref[grep(downto, rank_ref$ranks, ignore.case=T):nrow(rank_ref),"rankId"]
+	
+	stop_ <- "not" 
 	notout <- data.frame(rankName = "")
 	out <- list()
-	while(fam == "not"){
+	iter <- 0
+	while(stop_ == "not"){
+		iter <- iter + 1
 		if(!nchar(as.character(notout$rankName[[1]])) > 0){
 			temp <- ldply(tsns, gettaxonomicranknamefromtsn)
 		} else
 			{ temp <- notout }
 		tt <- ldply(temp$tsn, gethierarchydownfromtsn)
 		names_ <- ldply(split(tt, row.names(tt)), function(x) 
-			gettaxonomicranknamefromtsn(x$tsn)[,c("rankName","tsn")]) 
+			gettaxonomicranknamefromtsn(x$tsn)[,c("rankId","rankName","tsn")]) 
 		tt <- merge(tt[,-3], names_[,-1], by="tsn")
-		if(nrow(tt[tt$rankName == "Family", ]) > 0) out[[sample(lets, 1)]] <- tt[tt$rankName == "Family", ]
-		if(nrow(tt[!tt$rankName == "Family", ]) > 0) {
-			notout <- tt[!tt$rankName %in% c('Family','Subfamily','Tribe','Subtribe','Genus',
-				'Subgenus','Species','Subspecies','Variety','Infrakingdom','Division',
-				'Subdivision','Infradivision','Section','Subsection','Subvariety','Form',
-				'Subform','Race','Stirp','Morph','Aberration','Unspecified'
-			), ]
+		if(nrow(tt[tt$rankId == downto2, ]) > 0) out[[iter]] <- tt[tt$rankId == downto2, ]
+		if(nrow(tt[!tt$rankId == downto2, ]) > 0) {
+			notout <- tt[!tt$rankId %in% torank_ids, ]
 		} else
-			{ notout <- data.frame(rankName = "Family") }
+			{ notout <- data.frame(rankName = downto) }
 		
-		if(all(notout$rankName == "Family")) { 
-			fam <- "fam"
+		if(all(notout$rankName == downto)) { 
+			stop_ <- "fam"
 		} else
-			{ 
-				tsns <- notout$tsn
-				fam <- "not" 
-			}
+		{ 
+			tsns <- notout$tsn
+			stop_ <- "not" 
+		}
 	}
-	ldply(out)[,-1]
+	ldply(out)
 }
