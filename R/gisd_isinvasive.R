bb5ce0d41813406bc912776ccefa5298340a3865
EDiLD <szoe8822@uni-landau.de>
2012-12-12 20:04:28 +0100

    added roxygen documentation for gisd_isinvasive

--- R/gisd_isinvasive.R
+++ R/gisd_isinvasive.R
@@ -1,11 +1,43 @@
-gisd_isinvasive <- function(sp, simplify = TRUE){ 
+#' Check invasive species status for a set of species from GISD database
+#' 
+#' @import plyr XML RCurl
+#' 
+#' @param x character; a vector of scientific species names in the form of c("Genus species").
+#' @param simplify logical; returns a data.frame with the species name and the values "Invasive", "Not in GISD". 
+#' I recomend to check first the not simplified version (default), which contains raw information about the level of invasiveness.
+#' 
+#' @return A data.frame with species names and invasiveness.
+#' 
+#' @description This function check which species (both plants and animals) are 
+#' considered "invaders" somewhere in the world.  
+#' 
+#' For that end, it checks GISD (http://www.issg.org/database/welcome/) and returns a value,
+#' either "Not in GISD" or the brief description presented in GISD. 
+#' 
+#' Note that the webpage contains more information. Also note that the function won't tell 
+#' you if it's exotic in your area, a lot of exotic species are not considered invaders (yet). 
+#' 
+#' As expected, the function is as good as the database is, which I find quite reliable 
+#' and well maintained. 
+#' The database is also able to recognize a lot (but not all) of the species synonyms.
+#' 
+#' @author Ignasi Bartomeus \email{nacho.bartomeus@@gmail.com}
+#' @examples \dontrun{
+#' sp <- c("Carpobrotus edulis", "Rosmarinus officinalis")
+#' ## first species is invasive, second one is not. 
+#' is.invasive(sp)
+#' is.invasive(sp, simplified.df = TRUE)
+#' }
+#' 
+#' @export
+gisd_isinvasive <- function(x, simplify = FALSE){ 
 	# reformat sp list
-	species <- gsub(" ", "+", sp)
+	species <- gsub(" ", "+", x)
 	# create urls to parse
 	urls <- paste("http://www.issg.org/database/species/search.asp?sts=sss&st=sss&fr=1&x=13&y=9&sn=",
 								species, "&rn=&hci=-1&ei=-1&lang=EN", sep = "")
 	# create a data.frame to store the Output
-	out <- data.frame(species = sp, status = c(1:length(urls)))
+	out <- data.frame(species = x, status = c(1:length(urls)))
 	#loop through all species
 	for(i in 1:length(urls)){
 		#Parse url and extract table
