3a7313a31ca7763e89b691590a38e7d658c63068
EDiLD <szoe8822@uni-landau.de>
2012-12-12 19:39:28 +0100

    changed name, formatting, simplify = TRUE as default.

--- R/gisd_isinvasive.R
+++ R/gisd_isinvasive.R
@@ -1,14 +1,11 @@
-is.invasive <- function(sp, simplified.df = FALSE){ 
-	require(plyr)
-	require(XML)
-	require(RCurl)
-	#reformat sp list
-	species <- gsub(" ","+",sp)
-	#create urls to parse
+gisd_isinvasive <- function(sp, simplify = TRUE){ 
+	# reformat sp list
+	species <- gsub(" ", "+", sp)
+	# create urls to parse
 	urls <- paste("http://www.issg.org/database/species/search.asp?sts=sss&st=sss&fr=1&x=13&y=9&sn=",
-								species,"&rn=&hci=-1&ei=-1&lang=EN", sep = "")
-	#create a data.frame to store the Output
-	Out <- data.frame(species = sp, status = c(1:length(urls)))
+								species, "&rn=&hci=-1&ei=-1&lang=EN", sep = "")
+	# create a data.frame to store the Output
+	out <- data.frame(species = sp, status = c(1:length(urls)))
 	#loop through all species
 	for(i in 1:length(urls)){
 		#Parse url and extract table
@@ -16,15 +13,18 @@ is.invasive <- function(sp, simplified.df = FALSE){
 		tables <- getNodeSet(doc, "//table")
 		t <- readHTMLTable(tables[[4]])
 		tt <- as.matrix(t)
-		if(length(grep("No invasive species currently recorded",tt, value = TRUE)) > 0){
-			Out[i,2] <- "Not in GISD"
+		if(length(grep("No invasive species currently recorded", tt, value = TRUE)) > 0){
+			out[i, 2] <- "Not in GISD"
 		}
 		else{
-			if(simplified.df == FALSE){Out[i,2] <- tt[12,1]}
-			else{Out[i,2] <- "Invasive"}
+			if(simplify == FALSE){
+        out[i, 2] <- tt[12, 1]
+			} else {
+        out[i, 2] <- "Invasive"
+			}
 		}
 		message(paste("Checking species", i+1))	
 	}
 	message("Done")
-	Out
+	return(out)
 }
