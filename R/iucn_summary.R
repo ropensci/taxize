d529796a30ae72a034fabb98553dd2f4b1dda181
EDiLD <szoe8822@uni-landau.de>
2013-01-11 09:57:55 +0100

    iucn_summary docs

--- R/iucn_summary.R
+++ R/iucn_summary.R
@@ -3,7 +3,7 @@
 #' Get a summary from the IUCN Red List (\url{http://www.iucnredlist.org/}).
 #' 
 #' @import XML plyr
-#' @param sciname Scientific name. 
+#' @param sciname character; Scientific name. 
 #' @return A list (for every species one entry) of lists with the following items:
 #' \item{status}{Red List Category.}
 #' \item{history}{History of status.}
