45f9de1073de6e73feec0e397ab9c674026f0edb
EDiLD <szoe8822@uni-landau.de>
2013-01-11 11:01:24 +0100

    check if get_uid returns NA

--- inst/tests/test-get_uid.R
+++ inst/tests/test-get_uid.R
@@ -3,8 +3,11 @@ context("get_uid")
 
 test_that("get_uid returns the correct value", {
 	expect_that(get_uid(c("Chironomus riparius", "Chaetopteryx")), matches(c("315576","492549")))
+	expect_that(is.na(get_uid(c("Chironomus riparius", "aaa"))[2]), is_true())
 })
 
 test_that("get_uid returns the correct class", {
 	expect_that(get_uid(c("Chironomus riparius", "Chaetopteryx")), is_a("uid"))
 })
+
+
