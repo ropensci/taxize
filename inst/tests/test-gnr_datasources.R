79998e3edd203d298b30ffb8607541e5b3655af8
schamberlain <myrmecocystus@gmail.com>
2013-07-26 13:13:20 -0700

    removed tests folder

--- inst/tests/test-gnr_datasources.R
+++
@@ -1,6 +0,0 @@
-# tests for gnr_datasources fxn in taxize
-context("gnr_datasources")
-
-test_that("gnr_datasources returns the correct class", {
-	expect_that(gnr_datasources(), is_a("list"))
-})
--- inst/tests/test-tp_namereferences.R
+++
@@ -1,8 +0,0 @@
-# tests for tp_namereferences fxn in taxize
-context("tp_namereferences")
-
-ttt <- tp_namereferences(id = 25509881)
-
-test_that("tp_namereferences returns the correct class", {
-	expect_that(ttt, is_a("data.frame"))
-})
