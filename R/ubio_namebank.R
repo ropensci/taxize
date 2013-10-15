b7d7112bd8bd9a04e06568f8e53b74f150065e0a
schamberlain <myrmecocystus@gmail.com>
2013-10-15 09:49:33 -0700

    fixed decoding issue in ubio_namebank, closes #188

--- R/ubio_namebank.R
+++ R/ubio_namebank.R
@@ -33,6 +33,7 @@ ubio_namebank <- function(searchName = NULL, searchAuth = NULL, searchYear=NULL,
 	toget <- c("namebankID", "nameString", "fullNameString", "packageID", 
 						 "packageName", "basionymUnit", "rankID", "rankName")
 	temp2 <- lapply(toget, function(x) sapply(xpathApply(temp, paste("//", x, sep="")), xmlValue))
+  temp2[2:3] <- sapply(temp2[2:3], base64Decode)
 	out <- data.frame(do.call(cbind, temp2))
 	names(out) <- c("namebankID", "nameString", "fullNameString", "packageID", 
 									"packageName", "basionymUnit", "rankID", "rankName")
