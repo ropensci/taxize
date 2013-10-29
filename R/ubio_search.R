a27877196e9155987a0dc14d038acaa22d8782fd
schamberlain <myrmecocystus@gmail.com>
2013-10-29 10:02:31 -0700

    added two new ubio fxns, #189

--- R/ubio_search.R
+++ R/ubio_search.R
@@ -1,4 +1,4 @@
-#' Search uBio namebank.
+#' This function will return NameBankIDs that match given search terms
 #' 
 #' @import XML RCurl plyr
 #' @param searchName (string) - term to search within name string
@@ -12,27 +12,28 @@
 #' (default is 1000)
 #' @param keyCode Your uBio API key; loads from .Rprofile. If you don't have 
 #'    one, obtain one at http://www.ubio.org/index.php?pagename=form.
-#' @details Can't seem to get json format results along with specifiying an 
-#'    API key, so if you use json your key is not specified at the moment
-#' @return List or dataframe of XXXX.
+#' @param callopts Parameters passed on to httr::GET call.
+#' @return A data.frame.
 #' @examples \dontrun{
-#' ubio_namebank(searchName = 'elephant', sci = 1, vern = 0)
-#' ubio_namebank(searchName = 'Astragalus aduncus', sci = 1, vern = 0)
+#' ubio_search(searchName = 'elephant', sci = 1, vern = 0)
+#' ubio_search(searchName = 'Astragalus aduncus', sci = 1, vern = 0)
 #' }
 #' @export
-ubio_namebank <- function(searchName = NULL, searchAuth = NULL, searchYear=NULL, 
-		order = NULL, sci = NULL, vern = NULL, keyCode = NULL) 
+ubio_search <- function(searchName = NULL, searchAuth = NULL, searchYear=NULL, 
+		order = NULL, sci = NULL, vern = NULL, keyCode = NULL, callopts=list()) 
 {
 	url = "http://www.ubio.org/webservices/service.php"
 	keyCode <- getkey(keyCode, "ubioApiKey")
-	args <- 
-		compact(list('function' = 'namebank_search', searchName = searchName, searchAuth = searchAuth,
-								 searchYear = searchYear, order = order,
-								 sci = sci, vern = vern, keyCode = keyCode))
-	temp <- xmlParse(getForm(url, .params=args))
+	args <- compact(list('function' = 'namebank_search', searchName = searchName, 
+                       searchAuth = searchAuth,
+	                     searchYear = searchYear, order = order,
+	                     sci = sci, vern = vern, keyCode = keyCode))
+	tmp <- GET(url, query=args, callopts)
+	stop_for_status(tmp)
+	tt <- content(tmp)
 	toget <- c("namebankID", "nameString", "fullNameString", "packageID", 
 						 "packageName", "basionymUnit", "rankID", "rankName")
-	temp2 <- lapply(toget, function(x) sapply(xpathApply(temp, paste("//", x, sep="")), xmlValue))
+	temp2 <- lapply(toget, function(x) sapply(xpathApply(tt, paste("//", x, sep="")), xmlValue))
   temp2[2:3] <- sapply(temp2[2:3], base64Decode)
 	out <- data.frame(do.call(cbind, temp2))
 	names(out) <- c("namebankID", "nameString", "fullNameString", "packageID", 
