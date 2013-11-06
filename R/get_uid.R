842d8987782bba9ad6d69698f0d49c8337627046
EDiLD <szoe8822@uni-landau.de>
2013-11-06 12:47:31 +0100

    adress issue #196: User can turn off interactive mode of get_tsn and get_uid.

--- R/get_uid.R
+++ R/get_uid.R
@@ -4,6 +4,9 @@
 #' 
 #' @import plyr RCurl
 #' @param sciname character; scientific name.
+#' @param ask logical; should get_tsn be run in interactive mode? 
+#' If TRUE and more than one TSN is found for teh species, the user is asked for 
+#' input. If FALSE NA is returned for multiple matches.
 #' @param verbose logical; If TRUE the actual taxon queried is printed on the 
 #'    console.
 #' 
@@ -22,9 +25,14 @@
 #' # When not found
 #' get_uid("howdy")
 #' get_uid(c("Chironomus riparius", "howdy"))
+#' 
+#' # multiple matches
+#' get_uid('Dugesia')  # user prompt needed
+#' get_uid('Dugesia', ask = FALSE) # returns NA for multiple matches
+#' 
 #' }
-get_uid <- function(sciname, verbose = TRUE){
-  fun <- function(sciname) {
+get_uid <- function(sciname, ask = TRUE, verbose = TRUE){
+  fun <- function(sciname, ask, verbose) {
     if(verbose)
       message("\nRetrieving data for taxon '", sciname, "'\n")
     sciname <- gsub(" ", "+", sciname)
@@ -33,45 +41,49 @@ get_uid <- function(sciname, verbose = TRUE){
     # NCBI limits requests to three per second
     xml_result <- xmlParse(getURL(searchurl))
     Sys.sleep(0.33)
-    id <- xpathSApply(xml_result, "//IdList/Id", xmlValue)    
+    uid <- xpathSApply(xml_result, "//IdList/Id", xmlValue)    
     # not found on ncbi
-    if (length(id) == 0){
+    if (length(uid) == 0){
       message("Not found. Consider checking the spelling or alternate classification")
-      id <- "not found"
+      uid <- "not found"
     }
     # more than one found on ncbi -> user input
-    if(length(id) > 1){
-      baseurl <- "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=taxonomy"
-      ID <- paste("ID=", paste(id, collapse= ","), sep = "")
-      searchurl <- paste(baseurl, ID, sep = "&")
-      tt <- getURL(searchurl)
-      ttp <- xmlTreeParse(tt, useInternalNodes = TRUE)
-      df <- ldply(xmlToList(ttp), data.frame)
-      df <- df[df$Item..attrs != 'String', c(2,5, 7)]
-      names(df) <- c("UID", "Rank", "Division")
-      rownames(df) <- 1:nrow(df)
-      
-      # prompt
-      message("\n\n")
-      message("\nMore than one UID found for taxon '", sciname, "'!\n
-          Enter rownumber of taxon (other inputs will return 'NA'):\n")      
-      print(df)
-      take <- scan(n = 1, quiet = TRUE, what = 'raw')
-      
-      if(length(take) == 0)
-        take <- 'notake'
-      if(take %in% seq_len(nrow(df))){
-        take <- as.numeric(take)
-        message("Input accepted, took UID '", as.character(df$UID[take]), "'.\n")
-        id <- as.character(df$UID[take])
+    if(length(uid) > 1){
+      if(ask){
+        baseurl <- "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=taxonomy"
+        ID <- paste("ID=", paste(uid, collapse= ","), sep = "")
+        searchurl <- paste(baseurl, ID, sep = "&")
+        tt <- getURL(searchurl)
+        ttp <- xmlTreeParse(tt, useInternalNodes = TRUE)
+        df <- ldply(xmlToList(ttp), data.frame)
+        df <- df[df$Item..attrs != 'String', c(2,5, 7)]
+        names(df) <- c("UID", "Rank", "Division")
+        rownames(df) <- 1:nrow(df)
+        
+        # prompt
+        message("\n\n")
+        message("\nMore than one UID found for taxon '", sciname, "'!\n
+            Enter rownumber of taxon (other inputs will return 'NA'):\n")      
+        print(df)
+        take <- scan(n = 1, quiet = TRUE, what = 'raw')
+        
+        if(length(take) == 0)
+          take <- 'notake'
+        if(take %in% seq_len(nrow(df))){
+          take <- as.numeric(take)
+          message("Input accepted, took UID '", as.character(df$UID[take]), "'.\n")
+          uid <- as.character(df$UID[take])
+        } else {
+          uid <- NA
+          message("\nReturned 'NA'!\n\n")
+        }
       } else {
-        id <- NA
-        message("\nReturned 'NA'!\n\n")
+        uid <- NA
       }
     }  
-    return(id)
+    return(uid)
   }
-  out <- laply(sciname, fun)
+  out <- laply(sciname, fun, ask, verbose)
   class(out) <- "uid"
   return(out)
 }
