ff6d0fdddc507a60a88f4f3f2fe586bd064dcaa7
schamberlain <myrmecocystus@gmail.com>
2013-11-07 17:45:39 -0800

    moved tpl_search to be deprecated, will be removed in future version, closes #203

--- R/tpl_search.r
+++ R/tpl_search.r
@@ -1,5 +1,13 @@
 #' A light wrapper around the taxonstand fxn to call Theplantlist.org database.
 #' 
+#' This function is deprecated.
+#'
+#' This function was a very light wrapper around TPLck anyway - we don't see further
+#' reason to keep this function. Use the Taxonstand functions \code{\link[Taxonstand]{TPL}} 
+#' and \code{\link[Taxonstand]{TPLck}}, directly. However, we are keeping two functions
+#' that give you access to download Theplantlist.org data \code{\link[taxize]{tpl_get}} 
+#' and to get Theplantlist.org families \code{\link[taxize]{tpl_families}}.
+#'
 #' @import Taxonstand plyr
 #' @param taxon A taxonomic name, or a vector of names.
 #' @param paral Paralellize or not (logical). Which back-end package you use 
@@ -7,6 +15,7 @@
 #' 		include: snow, multicore, parallel, doMC, etc.
 #' @param ... Further arguments passed on to the TPL or TPLck function of Taxonstand.
 #' 		See \code{TPL} and \code{TPLck} for arguments.
+#' @seealso \code{\link[taxize]{tpl_get}}, \code{\link[taxize]{tpl_families}}
 #' @examples \donttest{
 #' # Regular non-parallel
 #' splist <- c("Heliathus annuus","Abies procera","Poa annua",
@@ -19,8 +28,11 @@
 #' tpl_search(taxon = "Microbryum curvicollum", corr = TRUE, max.distance=5)
 #' }
 #' @export
+#' @rdname tpl_search-deprecated
 tpl_search <- function(taxon, paral = FALSE, ...)
 {
+  .Deprecated(msg="This function is deprecated, and will be removed in a future version of this package. Use the Taxonstand functions TPL and TPLck, directly.")
+  
   if(paral){
     out <- llply(taxon, function(x) TPLck(x, ...), .parallel=TRUE)
     ldply(out)
