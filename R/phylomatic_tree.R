c578ea51214d98aff8edbeb64e1d97dd0d12bf71
schamberlain <myrmecocystus@gmail.com>
2013-12-11 07:00:39 -0800

    fixes to phylomatic_tree, #214

--- R/phylomatic_tree.R
+++ R/phylomatic_tree.R
@@ -1,65 +1,97 @@
-#' Format tree string, submit to Phylomatic, get newick or nexml tree.
+#' Query Phylomatic for a phylogenetic tree.
 #' 
 #' @import httr ape
 #' @param taxa Phylomatic format input of taxa names.
+#' @param taxnames If true, we get the family names for you to attach to your 
+#'    species names to send to Phylomatic API. If FALSE, you have to provide the 
+#'    strings in the right format.
 #' @param get 'GET' or 'POST' format for submission to the website.
 #' @param informat One of newick, nexml, or cdaordf. If using a stored tree, 
 #'    informat should always be newick.
 #' @param method One of phylomatic or convert
 #' @param storedtree One of R20120829 (Phylomatic tree R20120829 for plants), 
-#' 		smith2011 (Smith 2011, plants), or binindaemonds2007 (Bininda-Emonds 2007, 
-#'   	mammals).
+#'    smith2011 (Smith 2011, plants), or binindaemonds2007 (Bininda-Emonds 2007, 
+#'    mammals).
+#' @param taxaformat Only option is slashpath for now. Leave as is.
 #' @param outformat One of newick, nexml, or fyt.
 #' @param clean Return a clean tree or not.
+#' @param parallel Run in parallel or not.
 #' @details Use the web interface here http://phylodiversity.net/phylomatic/
-#' @return Newick formatted tree, or nexml xml object.
+#' @return Newick formatted tree or nexml text.
 #' @examples \dontrun{ 
 #' # Input taxonomic names
-#' taxa <- c("Collomia grandiflora", "Lilium lankongense", "Helianthus annuus")
-#' tree <- phylomatic_tree(taxa=taxa, get = 'POST', informat='newick', 
-#'    method = "phylomatic", storedtree = "smith2011", 
-#'    outformat = "newick", clean = "true")
+#' taxa <- c("Poa annua", "Phlox diffusa", "Helianthus annuus")
+#' tree <- phylomatic_tree(taxa=taxa, get = 'POST')
 #' plot(tree)
 #' 
 #' # Lots of names
-#' taxa <- c("Collomia grandiflora", "Lilium lankongense", "Phlox diffusa", 
-#'           "Iteadaphne caudata", "Nicotiana tomentosa", "Gagea sarmentosa")
-#' tree <- phylomatic_tree(taxa=taxa, get = 'POST', informat='newick', 
-#'                         method = "phylomatic", storedtree = "smith2011",
-#'                         outformat = "newick", clean = "true")
-#' plot(tree)
+#' taxa <- c("Poa annua", "Collomia grandiflora", "Lilium lankongense", "Phlox diffusa", 
+#' "Iteadaphne caudata", "Gagea sarmentosa", "Helianthus annuus")
+#' tree <- phylomatic_tree(taxa=taxa, get = 'POST')
+#' plot(tree, no.margin=TRUE)
 #' 
+#' # In parallel with parallel=TRUE speeds up dramatically by doing the 
+#' gathering of 
+#' # family names from NCBI in parallel
+#' registerDoMC(cores=4)
+#' phylomatic_tree(taxa=taxa, get='POST', parallel=TRUE)
+#'    
 #' # Output NeXML format
-#' taxa <- c("Gonocarpus leptothecus", "Gonocarpus leptothecus", "Impatiens davidis")
-#' out <- phylomatic_tree(taxa=taxa, get = 'POST', informat='newick', method = "phylomatic", 
-#'    storedtree = "smith2011", outformat = "nexml", clean = "true")
-#' library(RNeXML)
-#' read.nexml(out, type="nexml", asText=TRUE)
+#' taxa <- c("Gonocarpus leptothecus", "Gonocarpus leptothecus", "Lilium lankongense")
+#' out <- phylomatic_tree(taxa=taxa, get = 'POST', outformat = "nexml")
+#' out
+#' cat(out)
 #' }
 #' @export
 
-phylomatic_tree <- function(taxa, get = 'GET', informat = "newick", method = "phylomatic", 
-  storedtree = "smith2011", outformat = "newick", clean = "true")
+phylomatic_tree <- function(taxa, taxnames = TRUE, get = 'GET',
+  informat = "newick", method = "phylomatic", storedtree = "smith2011", 
+  taxaformat = "slashpath", outformat = "newick", clean = "true", 
+  parallel=FALSE)
 {
-  url <- "http://phylodiversity.net/phylomatic/pmws"
-  taxa <- sapply(taxa, function(x) gsub("\\s", "_", x), USE.NAMES=FALSE)
-  if (length(taxa) > 1) { taxa <- paste(taxa, collapse = "\n") } else { taxa <- taxa }  
-  args <- compact(list(taxa = taxa, informat = informat, method = method, 
-                       storedtree = storedtree, outformat = outformat, clean = clean))
-  get <- match.arg(get, choices=c("GET",'POST'))
-  out <- eval(parse(text=get))(url, query=args)
-  stop_for_status(out)
-  tt <- content(out, as="text")
+  url = "http://phylodiversity.net/phylomatic/pmws"
+  
+  if(taxnames){
+    if(parallel){ 
+      dat_ <- llply(taxa, itis_phymat_format, format='isubmit', .parallel=TRUE)
+    } else {
+      dat_ <- llply(taxa, itis_phymat_format, format='isubmit')
+    }
+    
+    checknas <- sapply(dat_, function(x) strsplit(x, "/")[[1]][1])
+    checknas2 <- checknas[grep("na", checknas)]
+    if(length(checknas2)>0)
+      stop(sprintf("A family was not found for the following taxa:\n %s \n\n try setting taxnmaes=FALSE, and passing in a vector of strings, like \n%s", 
+                   paste(sapply(dat_, function(x) strsplit(x, "/")[[1]][3])[grep("na", checknas)], collapse=", "),
+                   'phylomatic_tree(taxa = c("asteraceae/taraxacum/taraxacum_officinale", "ericaceae/gaylussacia/gaylussacia_baccata", "ericaceae/vaccinium/vaccinium_pallidum"), taxnames=FALSE, parallel=FALSE)'
+      ))
+    
+  } else { dat_ <- taxa }
+  
+  if (length(dat_) > 1) { dat_ <- paste(dat_, collapse = "\n") } else { dat_ <- dat_ }
+  
+  args <- compact(list(taxa = dat_, informat = informat, method = method, 
+                       storedtree = storedtree, taxaformat = taxaformat, 
+                       outformat = outformat, clean = clean))
+  
+  if (get == 'POST') {  
+    tt <- POST(url, query=args)
+  } else if (get == 'GET') {
+    tt <- GET(url, query=args)
+  } else
+  { stop("Error: get must be one of 'POST' or 'GET'") }
+  stop_for_status(tt)
+  out <- content(tt, as="text")
   
   outformat <- match.arg(outformat, choices=c("nexml",'newick'))
   getnewick <- function(x){
     tree <- gsub("\n", "", x[[1]])
     read.tree(text = colldouble(tree))
   }
-
+  
   switch(outformat, 
-         nexml = tt,
-         newick = getnewick(tt))
+         nexml = out,
+         newick = getnewick(out))
 }
 
 collapse_double_root <- function(y) {
---
+++ R/phylomatic_tree2.R
@@ -0,0 +1,65 @@
+#' Format tree string, submit to Phylomatic, get newick or nexml tree.
+#' 
+#' Newer input format to the API, doesn't work that well. Do use phylomatic_tree()
+#' 
+#' @import httr ape
+#' @param taxa Phylomatic format input of taxa names.
+#' @param get 'GET' or 'POST' format for submission to the website.
+#' @param informat One of newick, nexml, or cdaordf. If using a stored tree, 
+#'    informat should always be newick.
+#' @param method One of phylomatic or convert
+#' @param storedtree One of R20120829 (Phylomatic tree R20120829 for plants), 
+#' 		smith2011 (Smith 2011, plants), or binindaemonds2007 (Bininda-Emonds 2007, 
+#'   	mammals).
+#' @param outformat One of newick, nexml, or fyt.
+#' @param clean Return a clean tree or not.
+#' @details Use the web interface here http://phylodiversity.net/phylomatic/
+#' @return Newick formatted tree, or nexml xml object.
+#' @examples \dontrun{ 
+#' # Input taxonomic names
+#' taxa <- c("Collomia grandiflora", "Lilium lankongense", "Helianthus annuus")
+#' tree <- phylomatic_tree2(taxa=taxa, get = 'POST', informat='newick', 
+#'    method = "phylomatic", storedtree = "smith2011", 
+#'    outformat = "newick", clean = "true")
+#' plot(tree)
+#' 
+#' # Lots of names
+#' taxa <- c("Collomia grandiflora", "Lilium lankongense", "Phlox diffusa", 
+#'           "Iteadaphne caudata", "Nicotiana tomentosa", "Gagea sarmentosa")
+#' tree <- phylomatic_tree2(taxa=taxa, get = 'POST', informat='newick', 
+#'                         method = "phylomatic", storedtree = "smith2011",
+#'                         outformat = "newick", clean = "true")
+#' plot(tree)
+#' 
+#' # Output NeXML format
+#' taxa <- c("Gonocarpus leptothecus", "Gonocarpus leptothecus", "Impatiens davidis")
+#' out <- phylomatic_tree2(taxa=taxa, get = 'POST', informat='newick', method = "phylomatic", 
+#'    storedtree = "smith2011", outformat = "nexml", clean = "true")
+#' library(RNeXML)
+#' read.nexml(out, type="nexml", asText=TRUE)
+#' }
+#' @export
+
+phylomatic_tree2 <- function(taxa, get = 'GET', informat = "newick", method = "phylomatic", 
+  storedtree = "smith2011", outformat = "newick", clean = "true")
+{
+  url <- "http://phylodiversity.net/phylomatic/pmws"
+  taxa <- sapply(taxa, function(x) gsub("\\s", "_", x), USE.NAMES=FALSE)
+  if (length(taxa) > 1) { taxa <- paste(taxa, collapse = "\n") } else { taxa <- taxa }  
+  args <- compact(list(taxa = taxa, informat = informat, method = method, 
+                       storedtree = storedtree, outformat = outformat, clean = clean))
+  get <- match.arg(get, choices=c("GET",'POST'))
+  out <- eval(parse(text=get))(url, query=args)
+  stop_for_status(out)
+  tt <- content(out, as="text")
+  
+  outformat <- match.arg(outformat, choices=c("nexml",'newick'))
+  getnewick <- function(x){
+    tree <- gsub("\n", "", x[[1]])
+    read.tree(text = colldouble(tree))
+  }
+
+  switch(outformat, 
+         nexml = tt,
+         newick = getnewick(tt))
+}
