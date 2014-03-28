81405b263f314e5a5d397318e442bc6fe17f6301
Scott Chamberlain <myrmecocystus@gmail.com>
2014-03-27 19:06:16 -0500

    added iplant resolve fxn, closes #260

---
+++ R/iplant_resolve.R
@@ -0,0 +1,22 @@
+#' iplant resolution
+#' 
+#' @import httr RJSONIO
+#' @importFrom plyr compact 
+#' @export
+#' @param query Vector of names
+#' @param retrieve Specifies whether to retrieve all matches for the names submitted. One of 'best' 
+#' (retrieves only the single best match for each name submitted) or 'all' (retrieves all matches)
+#' @examples \dontrun{
+#' iplant_resolve(query=c("Helianthus annuus", "Homo sapiens"))
+#' }
+
+iplant_resolve <- function(query, retrieve='all', callopts=list()){
+  url <- "http://tnrs.iplantc.org/tnrsm-svc/matchNames"
+  query <- paste(query, collapse = ",")
+  args <- compact(list(names=query, retrieve=retrieve))
+  out <- GET(url, query=args, callopts)
+  stop_for_status(out)
+  tt <- content(out, as = "text")
+  res <- RJSONIO::fromJSON(tt)$items
+  return( res )
+}
