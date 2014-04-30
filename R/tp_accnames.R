c7e99e4ad1c9f409ed09c6d0fe4813c0f974ddd5
Scott Chamberlain <myrmecocystus@gmail.com>
2014-04-30 16:50:25 -0700

    fixed tp_accnames bug, closes #277

--- R/tp_accnames.R
+++ R/tp_accnames.R
@@ -1,16 +1,21 @@
 #' Return all accepted names for a taxon name with a given id.
 #' 
 #' @import XML RCurl RJSONIO plyr
+#' @export
 #' @param id the taxon identifier code 
 #' @param format return in json or xml format (defaults to json)
 #' @param output raw = json or xml; or df = data.frame 
 #' @param key Your Tropicos API key; loads from .Rprofile.
 #' @return List or dataframe.
 #' @examples \dontrun{
-#' tp_accnames(id = 25503923)
+#' head(tp_accnames(id = 25503923))
 #' tp_accnames(id = 25503923, output = 'raw')
+#' head(tp_accnames(id = 25538750))
+#' 
+#' # No accepted names found
+#' tp_accnames(id = 25509881)
 #' }
-#' @export
+
 tp_accnames <- function(id, format = 'json', output = 'df', key = NULL) 
 {
   url = 'http://services.tropicos.org/Name/'
@@ -19,27 +24,30 @@ tp_accnames <- function(id, format = 'json', output = 'df', key = NULL)
     urlget <- paste(url, id, '/AcceptedNames?apikey=', key, '&format=json', sep="")
     message(urlget)
     searchresults <- fromJSON(urlget)
-    } 
+  } 
   else {
     urlget <- paste(url, id, '/AcceptedNames?apikey=', key, '&format=xml', sep="")
     message(urlget)
     xmlout <- getURL(urlget)
     searchresults <- xmlToList(xmlTreeParse(xmlout))
-    }
+  }
   if(output == 'df') { 
-    getdata <- function(x) {
-      syn <- ldply(x[[1]])
-      syn$category <- rep("Synonym", nrow(syn))
-      acc <- ldply(x[[2]])
-      acc$category <- rep("Accepted", nrow(acc))
-      ref <- ldply(x[[3]])
-      ref$category <- rep("Reference", nrow(ref))
-      temp <- rbind(syn, acc, ref)
-      names(temp)[1:2] <- c('variable','value')
-      temp
+    if("Error" %in% names(searchresults[[1]])){ searchresults[[1]] } else {
+      ldply(searchresults, getdata)
     }
-    ldply(searchresults, getdata)
   } else { searchresults }
+}
+
+getdata <- function(x) {
+  syn <- ldply(x[[1]])
+  syn$category <- rep("Synonym", nrow(syn))
+  acc <- ldply(x[[2]])
+  acc$category <- rep("Accepted", nrow(acc))
+  ref <- ldply(x[[3]])
+  ref$category <- rep("Reference", nrow(ref))
+  temp <- rbind(syn, acc, ref)
+  names(temp)[1:2] <- c('variable','value')
+  temp
 }
 
 #' Return all accepted names for a taxon name with a given id.
