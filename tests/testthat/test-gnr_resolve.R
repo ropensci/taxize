1adf782acf1f1eea7b63689c529cfcbe3f3dac69
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-09 11:03:38 -0700

    fixed broken gnr_resolve test

--- tests/testthat/test-gnr_resolve.R
+++ tests/testthat/test-gnr_resolve.R
@@ -4,9 +4,12 @@ context("gnr_resolve")
 tmp <- gnr_resolve(names = c("Helianthus annuus", "Homo sapiens"))
 
 test_that("gnr_resolve returns the correct value", {
-	expect_that(ncol(tmp), equals(4))
+	expect_that(ncol(tmp[[1]]), equals(4))
 })
 
 test_that("gnr_resolve returns the correct class", {
-	expect_that(tmp, is_a("data.frame"))
+	expect_is(tmp, "list")
+	expect_is(tmp[[1]], "data.frame")
+	expect_is(tmp[[1]]$matched_name, "character")
+	expect_null(tmp[[2]])
 })
