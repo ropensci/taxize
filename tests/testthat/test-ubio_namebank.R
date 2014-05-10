7f374c8dd3b89a4458ab7a0ee320543325f15a41
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-10 09:26:16 -0700

    fixed some broken tests due to ubio changes

--- tests/testthat/test-ubio_namebank.R
+++ tests/testthat/test-ubio_namebank.R
@@ -4,10 +4,11 @@ context("ubio_namebank")
 out <- ubio_search(searchName = 'elephant', sci = 1, vern = 0)
 
 test_that("ubio_namebank returns the correct value", {
-	expect_that(as.numeric(as.character(out[ , 1])), matches("6938660"))
+	expect_that(out$scientific[ , 1], matches("6938660"))
 })
 
 test_that("ubio_namebank returns the correct class", {
-	expect_that(out, is_a("data.frame"))
-	expect_that(ncol(out), equals(8))
+  expect_that(out, is_a("list"))
+	expect_that(out$scientific, is_a("data.frame"))
+	expect_that(ncol(out$scientific), equals(8))
 })
--- tests/testthat/test-ubio_search.R
+++ tests/testthat/test-ubio_search.R
@@ -4,11 +4,11 @@ context("ubio_search")
 out <- ubio_search(searchName = 'elephant', sci = 1, vern = 0)
 
 test_that("ubio_search returns the correct dimensions", {
-	expect_that(dim(out), equals(c(1,8)))
+	expect_that(dim(out$scientific), equals(c(1,8)))
 })
 
 test_that("ubio_search returns the correct class", {
-	expect_that(out, is_a("data.frame"))
-	expect_that(out$basionymunit, is_a("factor"))
-	expect_that(out$rankname, is_a("factor"))
+	expect_that(out$scientific, is_a("data.frame"))
+	expect_that(out$scientific$basionymunit, is_a("character"))
+	expect_that(out$scientific$rankname, is_a("character"))
 })
