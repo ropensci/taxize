6ae05a8a8718f3fe07f9db1694734975918f0dfc
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-16 10:42:14 -0700

    updated eol_search to use httr and warn for bad status, no way to add number records, closes #200

--- R/eol_search.R
+++ R/eol_search.R
@@ -1,6 +1,6 @@
 #' Search for terms in EOL database.
 #' 
-#' @import RCurl plyr assertthat RJSONIO
+#' @import httr plyr assertthat RJSONIO
 #' @export
 #' @param terms search terms (character)
 #' @param page A maximum of 30 results are returned per page. This parameter allows 
@@ -19,31 +19,28 @@
 #' @param callopts Curl options passed on to getForm.
 #' @details It's possible to return JSON or XML with the EOL API. However, 
 #' 		this function only returns JSON for now. 
-#' @return JSON list object, or data.frame.
+#' @return A data frame.
 #' @examples \dontrun{
-#' eol_search('Homo')
+#' eol_search(terms='Homo')
 #' eol_search(terms='Salix')
 #' eol_search(terms='Ursus americanus luteolus')
 #' }
 
 eol_search <- function(terms, page=1, exact=NULL, filter_tid=NULL, filter_heid=NULL,
   filter_by_string=NULL, cache_ttl=NULL, key = NULL, callopts=list()) 
-{     
+{
   url = 'http://eol.org/api/search/1.0.json'
 	key <- getkey(key, "eolApiKey")
 	query <- gsub("\\s", "+", terms)
-  args <- compact(list(q=query,page=page,exact=exact,filter_by_taxon_concept_id=filter_tid,
-                       filter_by_hierarchy_entry_id=filter_heid, 
-                       filter_by_string=filter_by_string,cache_ttl=cache_ttl))
-#   callopts <- c(add_headers(`Content-type` = "application/json; charset=utf-8"), callopts)
-#   tt <- GET(url, query=args, callopts)
-#   stop_for_status(tt)
-  tt <- getForm(url, .params=args, .opts = callopts)
-  assert_that(attributes(tt)$`Content-Type`[1]=='application/json')
-  assert_that(attributes(tt)$`Content-Type`[[2]]=='utf-8')
-#   temp <- content(tt, as = "text", encoding = "utf-8")
-  res <- RJSONIO::fromJSON(tt, simplifyWithNames = FALSE, encoding = "utf-8")
-  if(res$totalResults == 0){
+  args <- taxize_compact(list(q=query,page=page,exact=exact,filter_by_taxon_concept_id=filter_tid,
+      filter_by_hierarchy_entry_id=filter_heid, filter_by_string=filter_by_string,
+      cache_ttl=cache_ttl))
+  tt <- GET(url, query=args, callopts)
+  warn_for_status(tt)
+  assert_that(tt$headers$`content-type`[1]=='application/json; charset=utf-8')
+  parsed <- content(tt, as = "text")
+  res <- RJSONIO::fromJSON(parsed, simplifyWithNames = FALSE, encoding = "utf-8")
+  if(res$totalResults == 0 | length(res$results)==0){
     data.frame(pageid=NA, name=NA, stringsAsFactors = FALSE)
   } else
   { 
