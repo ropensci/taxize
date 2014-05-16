96f9e3e8b88b4686c94be8a830f3da6f0dc5d1e7
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-16 09:36:50 -0700

    cleaned up iplant_resolve a bit, #258

--- R/iplant_resolve.R
+++ R/iplant_resolve.R
@@ -2,20 +2,31 @@
 #' 
 #' @import httr RJSONIO
 #' @export
-#' @param query Vector of names
+#' @param query Vector of one or more taxonomic names. (no common names)
 #' @param retrieve Specifies whether to retrieve all matches for the names submitted. One of 'best' 
 #' (retrieves only the single best match for each name submitted) or 'all' (retrieves all matches)
 #' @param callopts Curl options passed on to \code{httr::GET}
+#' @return A data frame
 #' @examples \dontrun{
 #' iplant_resolve(query=c("Helianthus annuus", "Homo sapiens"))
+#' iplant_resolve("Helianthusss")
+#' res <- iplant_resolve("Pooa")
+#' do.call(rbind, lapply(res, data.frame, stringsAsFactors = FALSE))
+#' 
+#' library("httr")
+#' iplant_resolve("Helianthusss", callopts=verbose())
+#' }
+#' @examples \donttest{
+#' # You can use the timeout function from httr to set a timeout for a given number of seconds
+#' iplant_resolve("Helianthusss", callopts=timeout(seconds = 0.1))
 #' }
 
 iplant_resolve <- function(query, retrieve='all', callopts=list()){
   url <- "http://tnrs.iplantc.org/tnrsm-svc/matchNames"
   query <- paste(query, collapse = ",")
-  args <- compact(list(names=query, retrieve=retrieve))
+  args <- taxize_compact(list(names=query, retrieve=retrieve))
   out <- GET(url, query=args, callopts)
-  stop_for_status(out)
+  warn_for_status(out)
   tt <- content(out, as = "text")
   res <- RJSONIO::fromJSON(tt)$items
   return( res )
