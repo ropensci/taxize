a095775a013ab9297ab2fcbb929c8f67d2ada5b5
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-03 12:15:18 -0700

    added fxn to retrieve external ID given input worms id

--- R/worms_search.R
+++ R/worms_search.R
@@ -125,6 +125,38 @@ worms_common <- function(ids=NULL, opts=NULL, update_iface=FALSE, ...)
   do.call(rbind, lapply(res, function(y) data.frame(unclass(y), stringsAsFactors = FALSE)))
 }
 
+#' Get external ID from Worms ID
+#' 
+#' @template worms_id
+#' @param type External ID source to get ID for. One of ncbi (default), tsn, bold, eol, 
+#' dyntaxa, fishbase, iucn or lsid. 
+#' @return Character class with ID and attributess for found or not, the source name, and a URL for 
+#' the taxon with the external source. No URL is given for lsid. 
+#' @examples \dontrun{
+#' worms_extid(ids=1080, type='ncbi')
+#' worms_extid(ids=278241, type='tsn')
+#' worms_extid(ids=278241, type='iucn')
+#' worms_extid(ids=282981, type='fishbase')
+#' worms_extid(ids=127186, type='bold')
+#' worms_extid(ids=278241, type='eol')
+#' worms_extid(ids=278241, type='lsid')
+#' worms_extid(ids=127186, type='dyntaxa')
+#' }
+worms_extid <- function(ids=NULL, type='ncbi', opts=NULL, update_iface=FALSE, ...)
+{
+  server <- 'http://www.marinespecies.org/aphia.php?p=soap'
+  upiface <- worms_update_iface(update_iface)
+  if(!is.null(upiface)) worms_iface <- iface
+  fxn <- worms_get_fxn('getExtIDbyAphiaID')
+  res <- fxn(AphiaID = ids, type = type, server = server, .opts = opts)
+  res <- if(length(res)==0) NA else res
+  clazz <- switch(type, ncbi='uid', tsn='tsn', bold='bold', eol='eol', dyntaxa='dyntaxa', fishbase='fishbase', iucn='iucn', lsid='lsid')
+  attr(res, "match") <- if(length(res)==0) 'not found' else 'found'
+  attr(res, "uri") <- get_uri(type, res)
+  class(res) <- clazz
+  return(res)
+}
+
 
 ##### helpers
 worms_update_iface <- function(update_iface=FALSE, wsdl_url="http://www.marinespecies.org/aphia.php?p=soap&wsdl=1")
@@ -136,3 +168,16 @@ worms_update_iface <- function(update_iface=FALSE, wsdl_url="http://www.marinesp
 }
 
 worms_get_fxn <- function(x) worms_iface@functions[[x]]
+
+get_uri <- function(x, y){
+  switch(x, 
+         ncbi=sprintf('http://www.ncbi.nlm.nih.gov/taxonomy/%s', y),
+         tsn=sprintf('http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=%s', y),
+         bold=sprintf('http://www.boldsystems.org/index.php/Taxbrowser_Taxonpage?taxid=%s', y),
+         eol=sprintf('http://eol.org/pages/%s/overview', y),
+         dyntaxa=sprintf('http://www.dyntaxa.se/Taxon/Info/%s?changeRoot=True', y),
+         fishbase=sprintf('http://www.fishbase.org/summary/%s', y),
+         iucn=sprintf('http://www.iucnredlist.org/details/%s/0', y),
+         lsid=NA,
+  )
+}
