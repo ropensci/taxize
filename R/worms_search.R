f4c634500872f8efcae9a008d78aa7ed6467453a
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-03 12:27:27 -0700

    added worms_sources fxn to get references for an id

--- R/worms_search.R
+++ R/worms_search.R
@@ -125,6 +125,23 @@ worms_common <- function(ids=NULL, opts=NULL, update_iface=FALSE, ...)
   do.call(rbind, lapply(res, function(y) data.frame(unclass(y), stringsAsFactors = FALSE)))
 }
 
+#' Get sources/references by ID
+#' 
+#' @template worms_id
+#' @examples \dontrun{
+#' worms_sources(ids=1080)
+#' worms_sources(ids=278241)
+#' }
+worms_sources <- function(ids=NULL, opts=NULL, update_iface=FALSE, ...)
+{
+  server <- 'http://www.marinespecies.org/aphia.php?p=soap'
+  upiface <- worms_update_iface(update_iface)
+  if(!is.null(upiface)) worms_iface <- iface
+  fxn <- worms_get_fxn('getSourcesByAphiaID')
+  res <- fxn(AphiaID = ids, server = server, .opts = opts)
+  do.call(rbind, lapply(res, function(y) data.frame(unclass(y), stringsAsFactors = FALSE)))
+}
+
 #' Get external ID from Worms ID
 #' 
 #' @template worms_id
