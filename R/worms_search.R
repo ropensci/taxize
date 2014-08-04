0c7ffc06d7ca921f9d4f45dd610ac53c05508175
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-04 07:03:50 -0700

    added worms_records fxn

--- R/worms_search.R
+++ R/worms_search.R
@@ -142,6 +142,46 @@ worms_sources <- function(ids=NULL, opts=NULL, update_iface=FALSE, ...)
   do.call(rbind, lapply(res, function(y) data.frame(unclass(y), stringsAsFactors = FALSE)))
 }
 
+#' Get records by ID, scientific name, common name, date, worms id, or external id. 
+#' @export
+#' @template worms_id
+#' @examples \dontrun{
+#' worms_records(scientific='Salmo')
+#' worms_records(scientific='Liopsetta glacialis')
+#' worms_records(common='salmon')
+#' worms_records(startdate='2014-06-01T00:00:00', enddate='2014-06-02T00:00:00')
+#' worms_records(id=1080)
+#' worms_records(extid=6830, type='ncbi')
+#' }
+worms_records <- function(scientific=NULL, common=NULL, id=NULL, extid=NULL, like=NULL, type=NULL,
+  marine_only=1, offset=NULL, startdate=NULL, enddate=NULL, opts=NULL, update_iface=FALSE, ...)
+{
+  server <- 'http://www.marinespecies.org/aphia.php?p=soap'
+  upiface <- worms_update_iface(update_iface)
+  if(!is.null(upiface)) worms_iface <- iface
+  endpt <- if(!is.null(common)){ 
+    'getAphiaRecordsByVernacular' 
+  } else if (!is.null(startdate)|!is.null(enddate)) { 
+    'getAphiaRecordsByDate' 
+  } else if(!is.null(extid)) {
+    'getAphiaRecordByExtID'
+  } else if(!is.null(id)){
+    'getAphiaRecordByID'
+  } else { 
+    if(length(scientific) > 1) 'getAphiaRecordsByNames' else 'getAphiaRecords'  
+  }
+  fxn <- worms_get_fxn(endpt)
+  res <- switch(endpt,
+    getAphiaRecords = fxn(scientificname = scientific, like = like, fuzzy = 'false', marine_only = marine_only, offset = 'false', server = server, .opts = opts, ...),
+    getAphiaRecordsByNames = fxn(scientificname = scientific, like = like, fuzzy = 'false', marine_only = marine_only, server = server, .opts = opts, ...),
+    getAphiaRecordsByVernacular = fxn(vernacular = common, like = like, offset = offset, server = server, .opts = opts, ...),
+    getAphiaRecordsByDate = fxn(startdate = startdate, enddate = enddate, marine_only = marine_only, offset = offset, server = server, .opts = opts, ...),
+    getAphiaRecordByID = fxn(AphiaID = id, server = server, .opts = opts, ...),
+    getAphiaRecordByExtID = fxn(id = extid, type = type, server = server, .opts = opts, ...)
+  )
+  do.call(rbind, lapply(res, function(y) data.frame(unclass(y), stringsAsFactors = FALSE)))
+}
+
 #' Get external ID from Worms ID
 #' @export
 #' @template worms_id
