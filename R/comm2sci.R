4ff05c7c6ccee2adfe42a3d7f7722cf382d15e45
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-04 15:01:08 -0700

    added worms to comm2sci fxn

--- R/comm2sci.R
+++ R/comm2sci.R
@@ -2,7 +2,7 @@
 #' 
 #' @param commnames One or more common names or partial names.
 #' @param db Data source, one of \emph{"eol"} (default), \emph{"itis"}, \emph{"tropicos"}
-#' or \emph{"ncbi"}.
+#' \emph{"ncbi"}, or \emph{"worms"}.
 #' @param itisby Search for common names across entire names (search, default),
 #'    at beginning of names (begin), or at end of names (end).
 #' @param simplify (logical) If TRUE, simplify output to a vector of names. If FALSE,
@@ -12,7 +12,7 @@
 #' @seealso \code{\link[taxize]{searchbycommonname}}, 
 #' \code{\link[taxize]{searchbycommonnamebeginswith}}, 
 #' \code{\link[taxize]{searchbycommonnameendswith}}, \code{\link[taxize]{eol_search}},
-#' \code{\link[taxize]{tp_search}}
+#' \code{\link[taxize]{tp_search}}, \code{\link[taxize]{worms_records}}
 #' @export
 #' @seealso \code{\link[taxize]{sci2comm}}
 #' @author Scott Chamberlain (myrmecocystus@@gmail.com)
@@ -22,6 +22,7 @@
 #' comm2sci(commnames='annual blue grass', db='tropicos')
 #' comm2sci(commnames=c('annual blue grass','tree of heaven'), db='tropicos')
 #' comm2sci(commnames=c('black bear', 'roe deer'))
+#' comm2sci(commnames=c('atlantic salmon','great white shark'), db='worms')
 #' 
 #' # Output easily converts to a data.frame with \code{\link[plyr]{ldply}}
 #' library(plyr)
@@ -74,13 +75,21 @@ comm2sci <- function(commnames, db='eol', itisby='search', simplify=TRUE, ...)
       as.character(tmp$scientificname)
     } else{ tmp }
   }
+  
+  worms2sci <- function(simplify, ...){
+    tmp <- worms_records(...)
+    if(simplify){
+      as.character(tmp$scientificname)
+    } else{ tmp }
+  }
 
   getsci <- function(nn, ...){
     switch(db, 
            eol = eol_search_(terms=nn, simplify, ...),
            itis = foo(nn, itisby, simplify, ...),
            tropicos = tp_search_(simplify, commonname = nn, ...),
-           ncbi = ncbi2sci(nn))
+           ncbi = ncbi2sci(nn),
+           worms = worms2sci(common=nn, simplify))
   }
   temp <- lapply(commnames, function(x) getsci(x, ...))
   names(temp) <- commnames
