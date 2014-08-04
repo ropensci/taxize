5f90d6885672c8e0518ce31383e9af1f5163f86c
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-04 13:52:49 -0700

    added worms into synonyms fxn

--- R/synonyms.R
+++ R/synonyms.R
@@ -1,16 +1,17 @@
 #' Retrieve synonyms from various sources given input taxonomic names or identifiers.
 #' 
 #' @param x character; taxons to query.
-#' @param db character; database to query. either \code{itis} or \code{tropicos}.
-#' @param id character; identifiers, returned by \code{\link[taxize]{get_tsn}} 
-#'    or \code{\link[taxize]{get_tpsid}}
+#' @param db character; database to query. either \code{itis}, \code{tropicos}, or \code{worms}.
+#' @param id character; identifiers, returned by \code{\link[taxize]{get_tsn}}, 
+#'    \code{\link[taxize]{get_tpsid}}, or \code{\link[taxize]{get_wormsid}}
 #' @param ... Other passed arguments.
 #' 
 #' @return A named list of data.frames with the synonyms of every supplied taxa.
 #' @note If IDs are supplied directly (not from the \code{get_*} functions) you 
 #'    must specify the type of ID.
 #' 
-#' @seealso \code{\link[taxize]{get_tsn}}, \code{\link[taxize]{get_tpsid}}
+#' @seealso \code{\link[taxize]{get_tsn}}, \code{\link[taxize]{get_tpsid}}, or 
+#' \code{\link[taxize]{get_wormsid}}
 #' 
 #' @export
 #' @examples \dontrun{
@@ -20,12 +21,17 @@
 #' synonyms("Poa annua", db="tropicos")
 #' synonyms("Pinus contorta", db="tropicos")
 #' synonyms(c("Poa annua",'Pinus contorta'), db="tropicos")
+#' synonyms("Salvelinus fontinalis", db='worms')
 #' 
-#' # Use methods for get_uid and get_tsn
+#' # Use methods for get_tsn, get_tpsid, or get_wormsid
 #' synonyms(get_tsn("Poa annua"))
+#' synonyms(get_tpsid("Poa annua"))
+#' synonyms(get_wormsid("Salvelinus fontinalis"))
 #' 
 #' # Pass many ids from class "ids"
 #' out <- get_ids(names="Poa annua", db = c('itis','tropicos'))
+#' synonyms(out)
+#' out <- get_ids(names="Salvelinus fontinalis", db = c('itis','worms'))
 #' synonyms(out)
 #' }
 
@@ -49,6 +55,11 @@ synonyms.default <- function(x, db = NULL, ...){
     out <- synonyms(id, ...)
     names(out) <- x
   }
+  if (db == 'worms') {
+    id <- get_wormsid(x, ...)
+    out <- synonyms(id, ...)
+    names(out) <- x
+  }
   return(out)
 }
 
@@ -67,7 +78,10 @@ synonyms.tsn <- function(id, ...)
     }
     out
   }
-  return( lapply(id, fun) )
+  tmp <- lapply(id, fun)
+  names(tmp) <- id
+  return(tmp)
+#   return( lapply(id, fun) )
 }
 
 #' @method synonyms tpsid
@@ -83,7 +97,29 @@ synonyms.tpsid <- function(id, ...)
     }
     out
   }
-  return( lapply(id, fun) )
+  tmp <- lapply(id, fun)
+  names(tmp) <- id
+  return(tmp)
+#   return( lapply(id, fun) )
+}
+
+#' @method synonyms wormsid
+#' @export
+#' @rdname synonyms
+synonyms.wormsid <- function(id, ...) 
+{
+  fun <- function(x){
+    if (is.na(x)) {
+      out <- NA
+    } else {
+      out <- worms_synonyms(x, ...)[,c("AphiaID","scientificname","status","rank","valid_AphiaID","valid_name")]
+    }
+    out
+  }
+  tmp <- lapply(id, fun)
+  names(tmp) <- id
+  return(tmp)
+#   return( lapply(id, fun) )
 }
 
 
