c0325cc4ecbcc538a8e2d44f87c3b85088d81ecb
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-06 18:22:03 -0700

    fixed param defs for bold_search fxn, #301

--- R/bold_search.R
+++ R/bold_search.R
@@ -5,12 +5,30 @@
 #' @param name (character) One or more scientific names.
 #' @param id (integer) One or more BOLD taxonomic identifiers.
 #' @param fuzzy (logical) Whether to use fuzzy search or not (default: FALSE).
-#' @param dataTypes (character) Specifies the datatypes that will be returned. 'all' returns all data.
-#' 'basic' returns basic taxon information. 'images' returns specimen images.
+#' @param dataTypes (character) Specifies the datatypes that will be returned. See Details for 
+#' options.
 #' @param includeTree (logical) If TRUE (default: FALSE), returns a list containing information
 #' for parent taxa as well as the specified taxon.
-#' @details You must provide one of name or id. The other parameters (format 
-#' 		and start) are optional.
+#' @param response (logical) Note that response is the object that returns from the Curl call,
+#' useful for debugging, and getting detailed info on the API call.
+#' @param ... Further args passed on to httr::GET, main purpose being curl debugging
+#' @details You must provide one of name or id. The other parameters are optional.
+#'   	
+#' Options for \code{dataTypes} parameter:
+#' 
+#' \itemize{
+#'  \item all returns all data
+#'  \item basic returns basic taxon information
+#'  \item images returns specimen image. Includes copyright information, image URL, image metadata.
+#'  \item stats Returns specimen and sequence statistics. Includes public species count, public BIN 
+#'  count, public marker counts, public record count, specimen count, sequenced specimen count, 
+#'  barcode specimen count, species count, barcode species count.
+#'  \item geo Returns collection site information. Includes country, collection site map.
+#'  \item sequencinglabs Returns sequencing labs. Includes lab name, record count.
+#'  \item depository Returns specimen depositories. Includes depository name, record count.
+#'  \item thirdparty Returns information from third parties. Includes wikipedia summary, wikipedia 
+#'  URL, GBIF map.
+#' }
 #' @references \url{http://webservice.catalogueoflife.org/}
 #' @return A list of data.frame's.
 #' @examples \dontrun{
