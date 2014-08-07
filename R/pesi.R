86221203f8e6acfe60fd4bcf9968c57bb2c4ecd3
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-07 08:26:58 -0700

    added missing doc file for pesi_iface, added some missing param defs for pesi fxns

--- R/pesi.R
+++ R/pesi.R
@@ -26,7 +26,8 @@ pesi_guid <- function(scientific=NULL, opts=NULL, iface=NULL, ...)
 #' @param ... Further args passed on to SSOAP
 #' @examples \dontrun{
 #' pesi_name(guid='67C3CC9C-624A-40C5-B63A-AB880E0300D1')
-#' pesi_name(guid=c('urn:lsid:marinespecies.org:taxname:105787','urn:lsid:marinespecies.org:taxname:105790'))
+#' pesi_name(guid=c('urn:lsid:marinespecies.org:taxname:105787','
+#'                  urn:lsid:marinespecies.org:taxname:105790'))
 #' }
 pesi_name <- function(guid=NULL, opts=NULL, iface=NULL, ...)
 {
@@ -41,6 +42,7 @@ pesi_name <- function(guid=NULL, opts=NULL, iface=NULL, ...)
 #' @param scientific One of more scientific names
 #' @param common One of more scientific names
 #' @param guid One or more PESI GUIDs
+#' @param like Argument...
 #' @param opts Options passed on to \code{SSOAP::.SOAP} for curl debugging.
 #' @param iface PESI SOAP interface. By default uses the cached version in taxize.
 #' @param ... Further args passed on to SSOAP
@@ -49,7 +51,8 @@ pesi_name <- function(guid=NULL, opts=NULL, iface=NULL, ...)
 #' pesi_records(common='arctic tern')
 #' pesi_records(common='great white shark')
 #' pesi_records(guid='67C3CC9C-624A-40C5-B63A-AB880E0300D1')
-#' pesi_records(guid=c('urn:lsid:marinespecies.org:taxname:105787','urn:lsid:marinespecies.org:taxname:105790'))
+#' pesi_records(guid=c('urn:lsid:marinespecies.org:taxname:105787',
+#'                    'urn:lsid:marinespecies.org:taxname:105790'))
 #' }
 pesi_records <- function(scientific=NULL, common=NULL, guid=NULL, like=NULL, opts=NULL, iface=NULL, ...)
 {
