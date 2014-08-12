1df4275305c7fab12f6f1d01d08e39697a19dcc5
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-11 18:42:28 -0700

    added ubio as a option in synonyms omnibus fxn, closes  #319

--- R/synonyms.R
+++ R/synonyms.R
@@ -1,9 +1,11 @@
 #' Retrieve synonyms from various sources given input taxonomic names or identifiers.
 #' 
 #' @param x character; taxons to query.
-#' @param db character; database to query. either \code{itis}, \code{tropicos}, or \code{worms}.
+#' @param db character; database to query. either \code{itis}, \code{tropicos}, \code{worms}
+#' or \code{ubio}.
 #' @param id character; identifiers, returned by \code{\link[taxize]{get_tsn}}, 
-#'    \code{\link[taxize]{get_tpsid}}, or \code{\link[taxize]{get_wormsid}}
+#'    \code{\link[taxize]{get_tpsid}}, \code{\link[taxize]{get_wormsid}},
+#'    or \code{\link[taxize]{get_ubioid}}
 #' @param ... Other passed arguments.
 #' 
 #' @return A named list of data.frames with the synonyms of every supplied taxa.
@@ -22,11 +24,14 @@
 #' synonyms("Pinus contorta", db="tropicos")
 #' synonyms(c("Poa annua",'Pinus contorta'), db="tropicos")
 #' synonyms("Salvelinus fontinalis", db='worms')
+#' synonyms("Salmo friderici", db='ubio')
+#' synonyms(c("Salmo friderici",'Carcharodon carcharias','Puma concolor'), db="ubio")
 #' 
 #' # Use methods for get_tsn, get_tpsid, or get_wormsid
 #' synonyms(get_tsn("Poa annua"))
 #' synonyms(get_tpsid("Poa annua"))
 #' synonyms(get_wormsid("Salvelinus fontinalis"))
+#' synonyms(get_ubioid("Carcharodon carcharias"))
 #' 
 #' # Pass many ids from class "ids"
 #' out <- get_ids(names="Poa annua", db = c('itis','tropicos'))
@@ -57,6 +62,11 @@ synonyms.default <- function(x, db = NULL, ...){
   }
   if (db == 'worms') {
     id <- get_wormsid(x, ...)
+    out <- synonyms(id, ...)
+    names(out) <- x
+  }
+  if (db == 'ubio') {
+    id <- get_ubioid(x, searchtype = 'scientific', ...)
     out <- synonyms(id, ...)
     names(out) <- x
   }
@@ -120,6 +130,24 @@ synonyms.wormsid <- function(id, ...)
   names(tmp) <- id
   return(tmp)
 #   return( lapply(id, fun) )
+}
+
+#' @method synonyms ubioid
+#' @export
+#' @rdname synonyms
+synonyms.ubioid <- function(id, ...) 
+{
+  fun <- function(x){
+    if (is.na(x)) {
+      out <- NA
+    } else {
+      out <- ubio_id(namebankID = x, ...)[['synonyms']]
+    }
+    out
+  }
+  tmp <- lapply(id, fun)
+  names(tmp) <- id
+  return(tmp)
 }
 
 
