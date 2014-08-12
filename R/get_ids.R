12ccb871d02703a47045c7b08f003fabd5b7d99b
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-11 20:12:26 -0700

    added ubio and pesi to get_ids fxn, closes #320

--- R/get_ids.R
+++ R/get_ids.R
@@ -4,25 +4,30 @@
 #' use other \code{get_*} functions to get identifiers from specific sources if 
 #' you like.
 #' 
+#' @export
 #' @param names character; Taxonomic name to query.
 #' @param db character; database to query. One or  more of \code{ncbi}, \code{itis}, 
-#'    \code{eol}, \code{col}, \code{tropicos}, \code{gbif}, \code{worms}
+#'    \code{eol}, \code{col}, \code{tropicos}, \code{gbif}, \code{worms}, \code{pesi},
+#'    or \code{ubio}
 #' @param ... Other arguments passed to \code{\link[taxize]{get_tsn}}, 
 #'    \code{\link[taxize]{get_uid}}, \code{\link[taxize]{get_eolid}}, 
 #'    \code{\link[taxize]{get_colid}}, \code{\link[taxize]{get_tpsid}},
-#'    \code{\link[taxize]{get_gbifid}}, or \code{\link[taxize]{get_wormsid}}.
+#'    \code{\link[taxize]{get_gbifid}}, \code{\link[taxize]{get_wormsid}},
+#'    \code{\link[taxize]{get_pesiid}}, or \code{\link[taxize]{get_ubioid}}.
 #' @return A vector of taxonomic identifiers, each retaining their respective S3
 #'    classes so that each element can be passed on to another function (see e.g.'s).
 #' @note There is a timeout of 1/3 seconds between queries to NCBI.
 #' @seealso \code{\link[taxize]{get_tsn}}, \code{\link[taxize]{get_uid}}, 
 #'    \code{\link[taxize]{get_eolid}}, \code{\link[taxize]{get_colid}}, 
 #'    \code{\link[taxize]{get_tpsid}}, \code{\link[taxize]{get_gbifid}}, or 
-#'    \code{\link[taxize]{get_wormsid}}.
-#' @export
+#'    \code{\link[taxize]{get_wormsid}}, \code{\link[taxize]{get_pesiid}}, or 
+#'    \code{\link[taxize]{get_ubioid}}.
 #' @examples \dontrun{
 #' # Plug in taxon names directly
 #' get_ids(names="Chironomus riparius", db = 'ncbi')
 #' get_ids(names="Salvelinus fontinalis", db = 'worms')
+#' get_ids(names="Salvelinus fontinalis", db = 'ubio')
+#' get_ids(names="Salvelinus fontinalis", db = 'pesi')
 #' get_ids(names="Salvelinus fontinalis", db = c('worms','ncbi'))
 #' get_ids(names=c("Chironomus riparius", "Pinus contorta"), db = 'ncbi')
 #' get_ids(names=c("Chironomus riparius", "Pinus contorta"), db = c('ncbi','itis'))
@@ -41,7 +46,7 @@ get_ids <- function(names, db = NULL, ...)
 {
   if(is.null(db))
     stop("Must specify on or more values for db!")
-  db <- match.arg(db, choices = c('itis','ncbi','eol','col','tropicos','gbif','worms'), several.ok = TRUE)
+  db <- match.arg(db, choices = c('itis','ncbi','eol','col','tropicos','gbif','worms','pesi','ubio'), several.ok = TRUE)
  
   foo <- function(x, names, ...){
     ids <- switch(x, 
@@ -51,7 +56,9 @@ get_ids <- function(names, db = NULL, ...)
                   col = get_colid(names, ...),
                   tropicos = get_tpsid(names, ...),
                   gbif = get_gbifid(names, ...),
-                  worms = get_wormsid(names, ...))
+                  worms = get_wormsid(names, ...),
+                  ubio = get_ubioid(names, ...),
+                  pesi = get_pesiid(names, ...))
     names(ids) <- names
     return( ids )
   }
