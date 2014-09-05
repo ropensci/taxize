aec235564b2fd77487c5c2d1a4c005db5d4832fc
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-05 08:15:31 -0700

    fixed internal error catching in tnrs(), #331 hopefully this is the end of it, but most likely not

--- R/tnrs.R
+++ R/tnrs.R
@@ -1,58 +1,58 @@
 #' Search the Phylotastic Taxonomic Name Resolution Service.
 #'
-#' Match taxonomic names using the Taxonomic Name Resolution Service (TNRS). 
+#' Match taxonomic names using the Taxonomic Name Resolution Service (TNRS).
 #' Returns score of the matched name, and whether it was accepted or not.
-#'  
+#'
 #' @import RCurl XML plyr stringr jsonlite data.table
 #' @export
-#' 
+#'
 #' @param query Quoted taxonomic names to search in a vector (character).
-#' @param source Specify the source you want to match names against. Defaults 
-#' 		to just retrieve data from all sources. Options: NCBI, iPlant_TNRS, 
+#' @param source Specify the source you want to match names against. Defaults
+#' 		to just retrieve data from all sources. Options: NCBI, iPlant_TNRS,
 #'   	or MSW3. Only available when using getpost="POST".
-#' @param code Nomenclatural code. One of: ICZN (zoological), ICN (algae, fungi, 
-#'    and plants), ICNB (bacteria), ICBN (botanical), ICNCP (cultivated plants), 
+#' @param code Nomenclatural code. One of: ICZN (zoological), ICN (algae, fungi,
+#'    and plants), ICNB (bacteria), ICBN (botanical), ICNCP (cultivated plants),
 #'    ICTV (viruses). Only available when using getpost="POST".
-#' @param getpost Use GET or POST method to send the query. If you have more 
-#'    than say 50 species or so in your query, you should probably use POST. 
-#'    IMPORTANT!!!!! -> 
-#'        POST is the only option for this parameter if you want to 
-#'        use source or code parameters. 
-#' @param sleep Numer of seconds by which to pause between calls. Defaults to 0 
+#' @param getpost Use GET or POST method to send the query. If you have more
+#'    than say 50 species or so in your query, you should probably use POST.
+#'    IMPORTANT!!!!! ->
+#'        POST is the only option for this parameter if you want to
+#'        use source or code parameters.
+#' @param sleep Numer of seconds by which to pause between calls. Defaults to 0
 #' 		seconds. Use when doing many calls in a for loop ar lapply type call.
 #' @param splitby Number by which to split species list for querying the TNRS.
 #' @param verbose Verbosity or not (default TRUE)
 #' @param callopts Curl debugging options to pass in httr::GET or POST
 #' @return data.frame of results from TNRS plus the name submitted.
-#' @details If there is no match in the Taxosaurus database, nothing is 
-#'    returned, so youwill not get anything back for non matches. 
+#' @details If there is no match in the Taxosaurus database, nothing is
+#'    returned, so youwill not get anything back for non matches.
 #' @examples \dontrun{
-#' # Default, uses GET curl method, you can't specify any other parameters when 
+#' # Default, uses GET curl method, you can't specify any other parameters when
 #' using GET
 #' mynames <- c("Panthera tigris", "Neotamias minimus", "Magnifera indica")
 #' tnrs(query = mynames)
-#' 
+#'
 #' # Specifying the source to match against
 #' mynames <- c("Helianthus annuus", "Poa annua")
 #' tnrs(query = mynames, source = "iPlant_TNRS")
-#' 
+#'
 #' # Specifying the nomenclatural code to match against
 #' mynames <- c("Helianthus annuus", "Poa annua")
 #' tnrs(query = mynames, code = "ICBN")
-#' 
+#'
 #' # You can specify multiple sources, by comma-separating them
-#' mynames <- c("Panthera tigris", "Eutamias minimus", "Magnifera indica", 
+#' mynames <- c("Panthera tigris", "Eutamias minimus", "Magnifera indica",
 #' "Humbert humbert")
 #' tnrs(query = mynames, source = "NCBI,MSW3")
-#' 
+#'
 #' # Using POST method, especially useful when you have a lot of species
-#' mynames <- c("Panthera tigris", "Eutamias minimus", "Magnifera indica", 
-#'    "Humbert humbert", "Helianthus annuus", "Pinus contorta", "Poa annua", 
-#'    "Abies magnifica", "Rosa california", "Festuca arundinace", 
+#' mynames <- c("Panthera tigris", "Eutamias minimus", "Magnifera indica",
+#'    "Humbert humbert", "Helianthus annuus", "Pinus contorta", "Poa annua",
+#'    "Abies magnifica", "Rosa california", "Festuca arundinace",
 #'    "Mimulus bicolor", "Sorbus occidentalis","Madia sativa", "Thymopsis thymodes",
 #'    "Bartlettia scaposa")
 #' tnrs(mynames, source = "NCBI")
-#' 
+#'
 #' # And even more names
 #' mynames <- names_list(rank="species", size=75)
 #' tnrs(query=mynames, source = "NCBI")
@@ -60,14 +60,14 @@
 #' tnrs(mynames, source = "NCBI", splitby=50)
 #' }
 
-tnrs <- function(query = NA, source = NULL, code = NULL, getpost = "POST", 
-                 sleep = 0, splitby = 30, verbose=TRUE, callopts=list())
+tnrs <- function(query = NA, source = NULL, code = NULL, getpost = "POST",
+                 sleep = 0, splitby = 30, verbose = TRUE, callopts = list())
 {
 	mainfunc <- function(x){
 	  url = "http://taxosaurus.org/submit"
-    
+
     Sys.sleep(time = sleep) # set amount of sleep to pause by
-		
+
 		if(getpost=="GET"){
 			if(!any(is.na(x))){
 				query2 <- paste(str_replace_all(x, ' ', '+'), collapse='%0A')
@@ -90,16 +90,16 @@ tnrs <- function(query = NA, source = NULL, code = NULL, getpost = "POST",
 			message <- jsonlite::fromJSON(tt, FALSE)[["message"]]
 			retrieve <- str_replace_all(str_extract(message, "http.+"), "\\.$", "")
 		}
-		
+
 		mssg(verbose, sprintf("Calling %s", retrieve))
-		
+
 		iter <- 0
 		output <- list()
 		timeout <- "wait"
 		while(timeout == "wait"){
 			iter <- iter + 1
       ss <- GET(retrieve, callopts)
-			error_handle(ss)
+			error_handle(ss, TRUE)
       temp <- jsonlite::fromJSON(content(ss, as="text"), FALSE)
 			if(grepl("is still being processed", temp["message"])==TRUE){timeout <- "wait"} else {
 				output[[iter]] <- temp
@@ -107,7 +107,7 @@ tnrs <- function(query = NA, source = NULL, code = NULL, getpost = "POST",
 			}
 		}
 		out <- compact(output)[[1]]
-		
+
 		# Parse results into data.frame
     df <- data.frame(rbindlist(lapply(out$names, parseres)))
     f <- function(x) str_replace_all(x, pattern="\\+", replacement=" ")
@@ -117,23 +117,23 @@ tnrs <- function(query = NA, source = NULL, code = NULL, getpost = "POST",
     df2 <- data.frame(apply(df2, c(1,2), function(x){
       if(grepl('\"', x)) gsub('\"', "", x) else x
     }), stringsAsFactors=FALSE)
-		
+
 		return(df2)
 	}
-	mainfunc_safe <- plyr::failwith(NULL, mainfunc)
-	
+# 	mainfunc_safe <- plyr::failwith(NULL, mainfunc)
+
   if(length(query) < 1 || is.na(query)) stop("Please supply at least one name", call. = FALSE)
-  
+
 	if(getpost == "GET" && length(query) > 75 | length(query) > 30 && getpost == "POST"){
-	  species_split <- slice(query, by = splitby)	
-	  
-	  out <- lapply(species_split, function(x) mainfunc_safe(x))
+	  species_split <- slice(query, by = splitby)
+
+	  out <- lapply(species_split, function(x) mainfunc(x))
 	  tmp <- data.frame(rbindlist(out))
 	  names(tmp) <- tolower(names(tmp))
 	  return( tmp )
 	} else
 	{
-	  tmp <- mainfunc_safe(query)
+	  tmp <- mainfunc(query)
 	  names(tmp) <- tolower(names(tmp))
 	  return( tmp )
 	}
@@ -142,8 +142,8 @@ tnrs <- function(query = NA, source = NULL, code = NULL, getpost = "POST",
 # Function to parse results
 parseres <- function(w){
   matches <- w$matches
-  foome <- function(z) { 
-    z[sapply(z, length)==0] <- "none" 
+  foome <- function(z) {
+    z[sapply(z, length)==0] <- "none"
     data.frame(z)
   }
   matches2 <- data.frame(rbindlist(lapply(matches, foome)))
@@ -159,20 +159,28 @@ slice <- function(input, by = 2) {
   lapply(tt, function(x) x[!is.na(x)])
 }
 
-error_handle <- function(x){
-  sc <- x$status_code
-  if(sc >= 400){ 
-    mssg <- switch(as.character(sc),
-                   '400' = "Bad Request. The request could not be understood by the server due to malformed syntax. The client SHOULD NOT repeat the request without modifications.",
-                   '401' = 'Unauthorized',
-                   '403' = 'Forbidden',
-                   '404' = 'Not Found',
-                   '500' = 'Internal Server Error. The server encountered an unexpected condition which prevented it from fulfilling the request.',
-                   '501' = 'Not Implemented',
-                   '502' = 'Bad Gateway',
-                   '503' = 'Service Unavailable',
-                   '504' = 'Gateway Timeout'
+error_handle <- function(x, checkcontent=FALSE){
+  tocheck <- x$status_code
+  if(checkcontent){
+    if( "metadata" %in% names(jsonlite::fromJSON(content(x), FALSE)) ){
+      codes <- sapply(jsonlite::fromJSON(content(x), FALSE)$metadata$sources, "[[", "status")
+      codes <- as.numeric(str_extract(codes, "[0-9]+"))
+      tocheck <- c(tocheck, codes)
+    }
+  }
+  if(any(tocheck >= 400)){
+    it <- tocheck[ tocheck >= 400 ]
+    mssg <- switch(as.character(it),
+       '400' = "Bad Request. The request could not be understood by the server due to malformed syntax. The client SHOULD NOT repeat the request without modifications.",
+       '401' = 'Unauthorized',
+       '403' = 'Forbidden',
+       '404' = 'Not Found',
+       '500' = 'Internal Server Error. The server encountered an unexpected condition which prevented it from fulfilling the request.',
+       '501' = 'Not Implemented',
+       '502' = 'Bad Gateway',
+       '503' = 'Service Unavailable',
+       '504' = 'Gateway Timeout'
     )
-    stop(sprintf("HTTP status %s - %s", sc, mssg), call. = FALSE)  
+    stop(sprintf("HTTP status %s - %s", it, mssg), call. = FALSE)
   }
 }
