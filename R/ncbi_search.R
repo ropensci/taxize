f60f8894c025609f53f5bb9f04ef9f228d3c0136
fosterz <zacharyfoster1989@gmail.com>
2014-09-29 16:46:54 -0700

    added ncbi_search arguments entrez_query and hypothetical and reorganized

--- R/ncbi_search.R
+++ R/ncbi_search.R
@@ -6,12 +6,16 @@
 #' @param limit Number of sequences to search for and return. Max of 10,000.
 #'    If you search for 6000 records, and only 5000 are found, you will of course
 #'    only get 5000 back.
+#' @param entrez_query (\code{character}; length 1) An Entrez-format query to filter results with. 
+#' @param hypothetical (\code{logical}; length 1) If \code{FALSE}, an attempt will be made to not
+#'   return hypothetical or predicted sequences judging from accession number prefixs. 
 #' @details Removes predicted sequences so you don't have to remove them.
 #'     Predicted sequences are those with accession numbers that have "XM_" or
 #' 		"XR_" prefixes.
 #' @return Data.frame of results.
 #' @seealso \code{\link[taxize]{ncbi_getbyid}}, \code{\link[taxize]{ncbi_getbyname}}
-#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}, Zachary Foster
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}, Zachary Foster 
+#'   \email{zacharyfoster1989@@gmail.com}
 #' @examples \donttest{
 #' # A single species
 #' out <- ncbi_search(taxa="Umbra limi", seqrange = "1:2000")
@@ -42,142 +46,127 @@
 #' }
 #' @export
 ncbi_search <- function(taxa = NULL, id = NULL, seqrange="1:3000", getrelated=FALSE, limit = 500,
-                        entrez_query = NULL, verbose=TRUE)
+                        entrez_query = NULL, hypothetical = FALSE, verbose=TRUE)
 {
-  # Argument validation 
-  if (sum(c(is.null(taxa), is.null(id))) != 1) {
-    stop("Either taxa or id must be specified, but not both")
-  }
-  if ((!is.null(id)) && getrelated) warning("option 'getrelated' has no effect when used with option 'id'.")
   
+  # Function to search for sequences with esearch
+  search_for_sequences <- function(id) {
+    # Contruct search query
+    query_term <- paste0("xXarbitraryXx[porgn:__txid", id, "] AND ", seqrange, " [SLEN]")
+    if (!is.null(entrez_query)) query_term <- paste(query_term, entrez_query, sep = " AND ")
+    query <- list(db = "nuccore", retmax = limit, term = query_term)
+    # Submit query to NCBI
+    query_init <- GET(url_esearch, query=query)
+    stop_for_status(query_init)
+    # Parse result
+    out <- xpathApply(content(query_init, as="parsed"), "//eSearchResult")[[1]]
+    return(out)
+  }
+
   # Function to parse results from http query
   parseres <- function(x){
     outsum <- xpathApply(content(x, as="parsed"), "//eSummaryResult")[[1]]
     names <- sapply(getNodeSet(outsum[[1]], "//Item"), xmlGetAttr, name="Name") # gets names of values in summary
     predicted <- as.character(sapply(getNodeSet(outsum, "//Item"), xmlValue)[str_detect(names, "Caption")]) #  get access numbers
-    predicted <- sapply(predicted, function(x) strsplit(x, "_")[[1]][[1]], USE.NAMES=FALSE)
+    has_access_prefix <- grepl("_", predicted)
+    access_prefix <- sapply(predicted,
+                            function(x) ifelse(has_access_prefix, strsplit(x, "_")[[1]][[1]], NA),
+                            USE.NAMES=FALSE)
+    predicted[has_access_prefix] <- sapply(predicted[has_access_prefix],
+                                            function(x) strsplit(x, "_")[[1]][[2]])
     length_ <- as.numeric(sapply(getNodeSet(outsum, "//Item"), xmlValue)[str_detect(names, "Length")]) # gets seq lengths
     gis <- as.numeric(sapply(getNodeSet(outsum, "//Item"), xmlValue)[str_detect(names, "Gi")]) # gets GI numbers
     spnames <- sapply(getNodeSet(outsum, "//Item"), xmlValue)[str_detect(names, "Title")] # gets seq lengths # get spp names
     spused <- sapply(spnames, function(x) paste(str_split(x, " ")[[1]][1:2], sep="", collapse=" "), USE.NAMES=FALSE)
     genesavail <- sapply(spnames, function(x) paste(str_split(x, " ")[[1]][-c(1:2)], sep="", collapse=" "), USE.NAMES=FALSE)
     df <- data.frame(spused=spused, length=length_, genesavail=genesavail, access_num=predicted, ids=gis, stringsAsFactors=FALSE)
-    return( df[!df$access_num %in% c("XM","XR"),] )
+    if (!hypothetical) df <- df[!access_prefix %in% c("XM","XR"),]
+    return(df)
   }
-
-  foo <- function(xx){
-    # Contruct search query
-    url_esearch <- "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
-    url_esummary <- "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi"
-    mssg(verbose, paste("Working on ", xx, "...", sep=""))
-    mssg(verbose, "...retrieving sequence IDs...")
-    if (is.null(id)) {
-      query_term <- paste(xx, "[Organism] AND", seqrange, "[SLEN]", collapse=" ")    
-    } else {
-      query_term <- paste0("xXarbitraryXx[porgn:__txid", xx, "] AND ", seqrange, " [SLEN]")
-    }
-    if (!is.null(entrez_query)) query_term <- paste(query_term, entrez_query, sep = " AND ")
-    query <- list(db = "nuccore", retmax = limit, term = query_term) 
-    # Submit query to NCBI
-    query_init <- GET(url_esearch, query=query)
-    stop_for_status(query_init)
-    # Parse result
-    out <- xpathApply(content(query_init, as="parsed"), "//eSearchResult")[[1]]
-    if (as.numeric(xmlValue(xpathApply(out, "//Count")[[1]])) == 0) { #if no results were found..
-      if (!is.null(id)) {
-        mssg(verbose, paste("no sequences for ", xx, sep=""))
-        return(NULL)
-      }
-      mssg(verbose, paste("no sequences for ", xx, " - getting other sp.", sep=""))
-      if(getrelated == FALSE){
-        mssg(verbose, paste("no sequences for ", xx, sep=""))
-        df <- data.frame(xx, NA, NA, NA, NA)
-      } else
-      {
-        mssg(verbose, "...retrieving sequence IDs for related species...")
-        newname <- strsplit(xx, " ")[[1]][[1]]
-        query_term <- paste(newname, "[Organism] AND", seqrange, "[SLEN]", collapse=" ")
-        if (!is.null(entrez_query)) query_term <- paste(query_term, entrez_query, sep = " AND ")
-        query <- list(db = "nuccore", retmax = limit, term = query_term)
-        query_init2 <- GET(url_esearch, query=query)
-        stop_for_status(query_init2)
-        out <- xpathApply(content(query_init2, "parsed"), "//eSearchResult")[[1]]
-        if( as.numeric(xmlValue(xpathApply(out, "//Count")[[1]]))==0 ){
-          mssg(verbose, paste("no sequences for ", xx, " or ", newname, sep=""))
-          df <- data.frame(xx, NA, NA, NA, NA)
-        }
-        {
-          ids <- xpathApply(out, "//IdList//Id") # Get sequence IDs in list
-          ids_ <- as.numeric(sapply(ids, xmlValue))  # Get sequence ID values
-          mssg(verbose, "...retrieving available genes and their lengths...")
-
-          actualnum <- length(ids_)
-          if(actualnum > 10000){
-            q <- list(db = "nucleotide")
-            getstart <- seq(from=1, to=actualnum, by=10000)
-            getnum <- c(rep(10000, length(getstart)-1), actualnum-sum(rep(10000, length(getstart)-1)))
-            iterlist = list()
-            for(i in seq_along(getstart)){
-              q$id = paste(ids_[getstart[i]:(getstart[i]+(getnum[i]-1))], collapse=" ")
-              q$retstart <- getstart[i]
-              q$retmax <- getnum[i]
-              query_res <- POST(url_esummary, body=q)
-              stop_for_status(query_res)
-              iterlist[[i]] <- parseres(query_res)
-            }
-            df <- data.frame(rbindlist(iterlist))
-          } else
-          {
-            q <- list(db = "nucleotide", id = paste(ids_, collapse=" "))
-            query_res <- POST(url_esummary, body=q)
-            stop_for_status(query_res)
-            df <- parseres(query_res)
-          }
-        }
-      }
-    } else
-    {
-      ids <- xpathApply(out, "//IdList//Id") # Get sequence IDs in list
-      ids_ <- as.numeric(sapply(ids, xmlValue))  # Get sequence ID values
-      mssg(verbose, "...retrieving available genes and their lengths...")
-
-      actualnum <- length(ids_)
-      if(actualnum > 10000){
-        q <- list(db = "nucleotide")
-        getstart <- seq(from=1, to=actualnum, by=10000)
-        getnum <- c(rep(10000, length(getstart)-1), actualnum-sum(rep(10000, length(getstart)-1)))
-        iterlist = list()
-        for(i in seq_along(getstart)){
-          q$id = paste(ids_[getstart[i]:(getstart[i]+(getnum[i]-1))], collapse=" ")
-          q$retstart <- getstart[i]
-          q$retmax <- getnum[i]
-          query_res <- POST(url_esummary, body=q)
-          stop_for_status(query_res)
-          iterlist[[i]] <- parseres(query_res)
-        }
-        df <- data.frame(rbindlist(iterlist))
-      } else
-      {
-        q <- list(db = "nucleotide", id = paste(ids_, collapse=" "))
+  
+  # Function to download sequence summary information using esummary
+  download_summary <- function(seq_id) {
+    actualnum <- length(seq_id)
+    if(actualnum > 10000){
+      q <- list(db = "nucleotide")
+      getstart <- seq(from=1, to=actualnum, by=10000)
+      getnum <- c(rep(10000, length(getstart)-1), actualnum-sum(rep(10000, length(getstart)-1)))
+      iterlist = list()
+      for(i in seq_along(getstart)){
+        q$id = paste(seq_id[getstart[i]:(getstart[i]+(getnum[i]-1))], collapse=" ")
+        q$retstart <- getstart[i]
+        q$retmax <- getnum[i]
         query_res <- POST(url_esummary, body=q)
         stop_for_status(query_res)
-        df <- parseres(query_res)
+        iterlist[[i]] <- parseres(query_res)
+      }
+      df <- data.frame(rbindlist(iterlist))
+    } else
+    {
+      q <- list(db = "nucleotide", id = paste(seq_id, collapse=" "))
+      query_res <- POST(url_esummary, body=q)
+      stop_for_status(query_res)
+      df <- parseres(query_res)
+    }
+    return(df)
+  }
+  
+  # Constants
+  url_esearch <- "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+  url_esummary <- "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi"
+
+  # Argument validation 
+  if (sum(c(is.null(taxa), is.null(id))) != 1) {
+    stop("Either taxa or id must be specified, but not both")
+  }
+  if ((!is.null(id)) && getrelated) warning("option 'getrelated' has no effect when used with option 'id'.")
+
+  # Convert 'taxa' to 'id' if 'taxa' is supplied
+  if (is.null(id)) {
+    id <- get_uid(taxa)
+  } else {
+    id <- as.character(id)
+    class(id) <- "uid"
+  }
+  
+  # Function to process queries one at a time
+  foo <- function(xx){
+    mssg(verbose, paste("Working on ", xx, "...", sep=""))
+    mssg(verbose, "...retrieving sequence IDs...")
+    esearch_result <- search_for_sequences(xx)
+    if (as.numeric(xmlValue(xpathApply(esearch_result, "//Count")[[1]])) == 0 && getrelated) {
+      mssg(verbose, paste("no sequences for ", xx, " - getting other sp.", sep=""))
+      query_classification <- classification(id = xx, db = "ncbi")[[1]]
+      if (nrow(query_classification) > 1) {
+        parent_name <- query_classification$name[nrow(query_classification) - 1]
+        mssg(verbose, paste0("...retrieving sequence IDs for ", parent_name, "..."))
+        xx <- get_uid(parent_name)
+        esearch_result <- search_for_sequences(parent_id)
+      } else {
+        mssg(verbose, paste0("no related taxa found"))        
       }
     }
+    if (as.numeric(xmlValue(xpathApply(esearch_result, "//Count")[[1]])) == 0) {
+      mssg(verbose, "no sequences found")
+      return(data.frame(xx, NA, NA, NA, NA))
+    }
+    seq_ids <- xpathSApply(esearch_result, "//IdList//Id", xmlValue) # Get sequence IDs in list
+    mssg(verbose, "...retrieving available genes and their lengths...")
+    df <- download_summary(seq_ids)    
     mssg(verbose, "...done.")
     if(nrow(df) < 1){
       df <- data.frame(taxon=NA,length=NA,gene_desc=NA,acc_no=NA,gi_no=NA)
     } else {
       names(df) <- c("taxon","length","gene_desc","acc_no","gi_no")
     }
-    return( df )
+    return(df)
   }
-
+  
   foo_safe <- plyr::failwith(NULL, foo)
-  if (length(c(taxa, id)) == 1) {
-    foo_safe(c(taxa, id))
+  if (length(id) == 1) {
+    foo_safe(id)
   } else {
-    lapply(c(taxa, id), foo_safe)
+    lapply(id, foo_safe)
   }
 }
 
