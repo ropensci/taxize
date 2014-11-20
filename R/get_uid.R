78bd0ce1bc384e99c45a6616a7fe51645e27a9b3
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-19 16:07:42 -0800

    added missing x param to get_uid

--- R/get_uid.R
+++ R/get_uid.R
@@ -1,37 +1,38 @@
 #' Get the UID codes from NCBI for taxonomic names.
-#' 
+#'
 #' Retrieve the Unique Identifier (UID) of a taxon from NCBI taxonomy browser.
-#' 
+#'
 #' @import plyr RCurl
 #' @param sciname character; scientific name.
-#' @param ask logical; should get_tsn be run in interactive mode? 
-#' If TRUE and more than one TSN is found for the species, the user is asked for 
+#' @param ask logical; should get_tsn be run in interactive mode?
+#' If TRUE and more than one TSN is found for the species, the user is asked for
 #' input. If FALSE NA is returned for multiple matches.
-#' @param verbose logical; If TRUE the actual taxon queried is printed on the 
+#' @param verbose logical; If TRUE the actual taxon queried is printed on the
 #'    console.
-#' 
-#' @return A vector of unique identifiers (UID). If a taxon is not found NA. 
-#' If more than one UID is found the function asks for user input (if ask = TRUE), 
-#' otherwise returns NA. Comes with an attribute \emph{match} to investigate the 
+#' @param x Input to as.uid
+#'
+#' @return A vector of unique identifiers (UID). If a taxon is not found NA.
+#' If more than one UID is found the function asks for user input (if ask = TRUE),
+#' otherwise returns NA. Comes with an attribute \emph{match} to investigate the
 #' reason for NA (either 'not found', 'found' or if ask = FALSE 'multi match')
-#' 
+#'
 #' @seealso \code{\link[taxize]{get_tsn}}, \code{\link[taxize]{classification}}
-#' 
+#'
 #' @export
 #' @author Eduard Szoecs, \email{eduardszoecs@@gmail.com}
-#' 
+#'
 #' @examples \donttest{
 #' get_uid(c("Chironomus riparius", "Chaetopteryx"))
 #' get_uid(c("Chironomus riparius", "aaa vva"))
-#' 
+#'
 #' # When not found
 #' get_uid("howdy")
 #' get_uid(c("Chironomus riparius", "howdy"))
-#' 
+#'
 #' # multiple matches
 #' get_uid('Dugesia')  # user prompt needed
 #' get_uid('Dugesia', ask = FALSE) # returns NA for multiple matches
-#' 
+#'
 #' # Go to a website with more info on the taxon
 #' res <- get_uid("Chironomus riparius")
 #' browseURL(attr(res, "uri"))
@@ -41,12 +42,12 @@ get_uid <- function(sciname, ask = TRUE, verbose = TRUE){
   fun <- function(sciname, ask, verbose) {
     mssg(verbose, "\nRetrieving data for taxon '", sciname, "'\n")
     sciname <- gsub(" ", "+", sciname)
-    searchurl <- paste("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=taxonomy&term=", 
+    searchurl <- paste("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=taxonomy&term=",
                        sciname, sep = "")
     # NCBI limits requests to three per second
     xml_result <- xmlParse(getURL(searchurl))
     Sys.sleep(0.33)
-    uid <- xpathSApply(xml_result, "//IdList/Id", xmlValue) 
+    uid <- xpathSApply(xml_result, "//IdList/Id", xmlValue)
     att <- 'found'
     # not found on ncbi
     if (length(uid) == 0){
@@ -66,14 +67,14 @@ get_uid <- function(sciname, ask = TRUE, verbose = TRUE){
         df <- df[df$Item..attrs != 'String', c(2,5, 7)]
         names(df) <- c("UID", "Rank", "Division")
         rownames(df) <- 1:nrow(df)
-        
+
         # prompt
         message("\n\n")
         message("\nMore than one UID found for taxon '", sciname, "'!\n
-            Enter rownumber of taxon (other inputs will return 'NA'):\n")      
+            Enter rownumber of taxon (other inputs will return 'NA'):\n")
         print(df)
         take <- scan(n = 1, quiet = TRUE, what = 'raw')
-        
+
         if(length(take) == 0)
           take <- 'notake'
         if(take %in% seq_len(nrow(df))){
@@ -90,7 +91,7 @@ get_uid <- function(sciname, ask = TRUE, verbose = TRUE){
         uid <- NA
         att <- 'multi match'
       }
-    }  
+    }
     return(data.frame(uid, att, stringsAsFactors= FALSE))
   }
   sciname <- as.character(sciname)
@@ -99,7 +100,7 @@ get_uid <- function(sciname, ask = TRUE, verbose = TRUE){
   attr(out, 'match') <- outd$att
   if(!is.na(out[1])){
     urlmake <- na.omit(out)
-    attr(out, 'uri') <- 
+    attr(out, 'uri') <-
       sprintf('http://www.ncbi.nlm.nih.gov/taxonomy/%s', urlmake)
   }
   class(out) <- "uid"
@@ -119,7 +120,7 @@ as.uid.uid <- function(x) x
 as.uid.character <- function(x){
   if(check_uid(x)){
     uri <- sprintf('http://www.ncbi.nlm.nih.gov/taxonomy/%s', x)
-    structure(x, class="uid", match="found", uri=uri) 
+    structure(x, class="uid", match="found", uri=uri)
   } else { structure(x, class="uid", match="not found")   }
 }
 
