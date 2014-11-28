a90854ee9d9d2f5383495dd85a2f7c3a418671b8
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-28 12:39:44 -0800

    fixes to itis_downstream, was broken

--- R/itis_downstream.R
+++ R/itis_downstream.R
@@ -1,43 +1,41 @@
 #' Retrieve all taxa names or TSNs downstream in hierarchy from given TSN.
-#' 
+#'
 #' @import XML RCurl plyr
 #' @export
-#' @param tsns A taxonomic serial number. 
+#' @param tsns A taxonomic serial number.
 #' @param downto The taxonomic level you want to go down to. See examples below.
-#' 		The taxonomic level IS case sensitive, and you do have to spell it 
+#' 		The taxonomic level IS case sensitive, and you do have to spell it
 #' 		correctly. See \code{data(rank_ref)} for spelling.
-#' @param verbose logical; If TRUE (default), informative messages printed.
-#' @return Data.frame of taxonomic information downstream to family from e.g., 
-#' 		Order, Class, etc. 
+#' @return Data.frame of taxonomic information downstream to family from e.g.,
+#' 		Order, Class, etc.
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @examples \donttest{
 #' itis_downstream(tsns = 846509, downto="Genus")
-#' 
+#'
 #' # getting families downstream from Acridoidea
 #' itis_downstream(tsns = 650497, "Family")
-#' 
+#'
 #' # getting species downstream from Ursus
 #' itis_downstream(tsns = 180541, "Species")
 #' }
 
-itis_downstream <- function(tsns, downto, verbose=TRUE) 
+itis_downstream <- function(tsns, downto, ...)
 {
 	downto2 <- rank_ref[grep(downto, rank_ref$ranks),"rankId"]
 	torank_ids <- rank_ref[grep(downto, rank_ref$ranks):nrow(rank_ref),"rankId"]
-	
-	stop_ <- "not" 
+
+	stop_ <- "not"
 	notout <- data.frame(rankName = "")
 	out <- list()
 	iter <- 0
 	while(stop_ == "not"){
 		iter <- iter + 1
 		if(!nchar(as.character(notout$rankName[[1]])) > 0){
-			temp <- ldply(tsns, function(z) gettaxonomicranknamefromtsn(z, verbose=verbose))
-		} else
-			{ temp <- notout }
-		tt <- ldply(temp$tsn, function(z) gethierarchydownfromtsn(z, verbose=verbose))
-		names_ <- ldply(split(tt, row.names(tt)), function(x) 
-			gettaxonomicranknamefromtsn(x$tsn, verbose=verbose)[,c("rankId","rankName","tsn")])
+			temp <- ldply(tsns, function(z) gettaxonomicranknamefromtsn(z, ...))
+		} else { temp <- notout }
+		tt <- ldply(temp$tsn, function(z) gethierarchydownfromtsn(z, ...))
+		names_ <- ldply(split(tt, row.names(tt)), function(x)
+			gettaxonomicranknamefromtsn(as.character(x$tsn), ...)[,c("rankId","rankName","tsn")])
 		if(nrow(names_) == 0){
 		  out[[iter]] <- data.frame(tsn="No data",parentname="No data",parenttsn="No data",taxonname="No data",rankid="No data",rankname="No data")
 		  stop_ <- "nodata"
@@ -46,20 +44,19 @@ itis_downstream <- function(tsns, downto, verbose=TRUE)
 		  if(nrow(tt[tt$rankId == downto2, ]) > 0) out[[iter]] <- tt[tt$rankId == downto2, ]
 		  if(nrow(tt[!tt$rankId == downto2, ]) > 0) {
 		    notout <- tt[!tt$rankId %in% torank_ids, ]
-		  } else { 
-		    notout <- data.frame(rankName = downto) 
+		  } else {
+		    notout <- data.frame(rankName = downto)
 		  }
-		  
-		  if(all(notout$rankName == downto)) { 
+
+		  if(all(notout$rankName == downto)) {
 		    stop_ <- "fam"
 		  } else
-		  { 
+		  {
 		    tsns <- notout$tsn
-		    stop_ <- "not" 
+		    stop_ <- "not"
 		  }
 		}
 	}
-	tmp <- ldply(out)
-  names(tmp) <- tolower(names(tmp))
-  tmp
+  tmp <- ldply(out)
+	setNames(tmp, tolower(names(tmp)))
 }
