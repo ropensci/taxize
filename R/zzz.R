e76fdf6c3edd0e049b54207a0adffddaab2def07
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-28 14:14:09 -0800

    added missing helper fxn colclasses

--- R/zzz.R
+++ R/zzz.R
@@ -200,3 +200,15 @@ sub_vector <- function(x, rows){
 }
 
 nstop <- function(x, arg='db') if (is.null(x)) stop(sprintf("Must specify %s!", arg), call. = FALSE)
+
+colClasses <- function(d, colClasses) {
+  colClasses <- rep(colClasses, len=length(d))
+  d[] <- lapply(seq_along(d), function(i) switch(colClasses[i],
+                                                 numeric=as.numeric(d[[i]]),
+                                                 character=as.character(d[[i]]),
+                                                 Date=as.Date(d[[i]], origin='1970-01-01'),
+                                                 POSIXct=as.POSIXct(d[[i]], origin='1970-01-01'),
+                                                 factor=as.factor(d[[i]]),
+                                                 as(d[[i]], colClasses[i]) ))
+  d
+}
