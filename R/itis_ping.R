ee071bd3f8a0e5c05682d535425f11f5986ca6a5
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-14 15:12:39 -0800

    allow passing in ... curl options to itis_ping fnx

--- R/itis_ping.R
+++ R/itis_ping.R
@@ -1,10 +1,10 @@
 #' Ping the ITIS API to see if it's working.
 #'
 #' And provides number of scientific and common names in a string.
-#'
+#' @export
+#' @param ... Curl options passed on to \code{\link[httr]{GET}}
+#' @seealso \code{\link{eol_ping}}
 #' @examples \dontrun{
 #' itis_ping()
 #' }
-#' @seealso \code{\link{eol_ping}}
-#' @export
-itis_ping <- function() getdescription()
+itis_ping <- function(...) getdescription(...)
