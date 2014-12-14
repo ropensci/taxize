6a5e0e24240c2066b051e8a850fab14449aa43b3
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-14 11:44:52 -0800

    no more startup message

--- R/startup.R
+++ R/startup.R
@@ -1,3 +1,3 @@
-.onAttach <- function(...) {
-  packageStartupMessage("\n\nNew to taxize? Tutorial at http://ropensci.org/tutorials/taxize_tutorial.html \ncitation(package='taxize') for the citation for this package \nAPI key names have changed. Use tropicosApiKey, eolApiKey, ubioApiKey, and pmApiKey in your .Rprofile file. \nUse suppressPackageStartupMessages() to suppress these startup messages in the future\n")
-} 
+# .onAttach <- function(...) {
+#   packageStartupMessage("\n\nNew to taxize? Tutorial at http://ropensci.org/tutorials/taxize_tutorial.html \ncitation(package='taxize') for the citation for this package \nAPI key names have changed. Use tropicosApiKey, eolApiKey, ubioApiKey, and pmApiKey in your .Rprofile file. \nUse suppressPackageStartupMessages() to suppress these startup messages in the future\n")
+# }
