fc97311d21a6ca4b70b6715629367b822f02045e
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-18 17:07:40 -0800

    removed startup.r file

--- R/startup.R
+++
@@ -1,3 +0,0 @@
-# .onAttach <- function(...) {
-#   packageStartupMessage("\n\nNew to taxize? Tutorial at http://ropensci.org/tutorials/taxize_tutorial.html \ncitation(package='taxize') for the citation for this package \nAPI key names have changed. Use tropicosApiKey, eolApiKey, ubioApiKey, and pmApiKey in your .Rprofile file. \nUse suppressPackageStartupMessages() to suppress these startup messages in the future\n")
-# }
