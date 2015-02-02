7afc70835fead4691fdda94f359fc053217f698a
Scott Chamberlain <myrmecocystus@gmail.com>
2015-02-02 11:58:09 -0800

    fixed tiny thing in col_downstream test

--- tests/testthat/test-col_downstream.R
+++ tests/testthat/test-col_downstream.R
@@ -19,14 +19,14 @@ test_that("col_downstream returns the correct class", {
   expect_that(temp5, is_a("list"))
   expect_that(temp6, is_a("list"))
   expect_that(temp12, is_a("list"))
-  expect_that(temp2[[1]], is_a("data.frame"))
+  expect_that(temp1[[1]], is_a("data.frame"))
   expect_named(temp3)
   expect_identical(temp3, col_downstream(name="Helianthus", downto = "Species", verbose = FALSE))
   expect_equal(as.character(temp4[[1]][,3][[1]]), "Phylum")
 })
 
 test_that("gives what's expected on input errors", {
-  library(plyr)
+  library("plyr")
   expect_message(col_downstream(name="Pinus contorta", downto = "Species")[[1]], "Try adjusting")
 #   expect_is(col_downstream(name=c("Buteo","Puma"), downto = "Family"), "list")
 #   expect_message(col_downstream(name=c("Buteo","Puma"), downto = "Family"), "Try adjusting")
