87d93a3d543dab2869189c8d34dd4e8ec13be320
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-10 09:55:43 -0700

    removed equality testing in eol_dataobjects fxn test

--- tests/testthat/test-eol_dataobjects.R
+++ tests/testthat/test-eol_dataobjects.R
@@ -3,11 +3,6 @@ context("eol_dataobjects")
 
 temp <- eol_dataobjects(id="d72801627bf4adf1a38d9c5f10cc767f", verbose=FALSE)
 
-test_that("eol_dataobjects returns the correct dimensions", {
-	expect_equal(length(temp), 8)
-	expect_equal(NCOL(temp$dataObjects), 22)
-})
-
 test_that("eol_dataobjects returns the correct class", {
 	expect_is(temp, "list")
 	expect_is(temp$scientificName, "character")
