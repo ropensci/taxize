d9ce17daeffda1c5d06bd1084b0c065d881128e1
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-01 07:04:48 -0700

    fixed a test, eol wasnt working

--- tests/testthat/test-sci2comm.R
+++ tests/testthat/test-sci2comm.R
@@ -1,8 +1,8 @@
 # tests for sci2comm fxn in taxize
 context("sci2comm")
 
-tt <- sci2comm(scinames='Helianthus annuus')
-uu <- sci2comm(scinames='Helianthus annuus', db='itis', verbose=FALSE)
+tt <- sci2comm(scinames = 'Helianthus annuus', db = 'ncbi')
+uu <- sci2comm(scinames = 'Helianthus annuus', db = 'itis', verbose = FALSE)
 
 test_that("sci2comm returns the correct value", {
   expect_that(names(tt), equals('Helianthus annuus'))
