c8c7058d1afb07ff82212acde76bcb86ddb4e701
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-01 07:05:32 -0700

    changed gnr_resolve to not wait for queued job, but to just chunk request to bits of 500

--- R/gnr_resolve.R
+++ R/gnr_resolve.R
@@ -57,12 +57,13 @@
 
 gnr_resolve <- function(names, data_source_ids = NULL, resolve_once = FALSE,
   with_context = FALSE, stripauthority = FALSE, highestscore = TRUE, best_match_only = FALSE,
-  preferred_data_sources = NULL, http="get", callopts=list())
-{
+  preferred_data_sources = NULL, http="get", ...) {
+
+  http <- match.arg(http, c("get","post"))
   num = NULL
   url <- "http://resolver.globalnames.org/name_resolvers.json"
   names2 <- paste0(names, collapse = "|")
-  if(length(names2) > 300 & http == "get") http <- "post"
+  if(length(names) > 300 && http == "get") http <- "post"
 
   data_source_ids <- paste0(data_source_ids, collapse = "|")
   preferred_data_sources <- paste0(preferred_data_sources, collapse = "|")
@@ -76,35 +77,28 @@ gnr_resolve <- function(names, data_source_ids = NULL, resolve_once = FALSE,
             resolve_once=resolve_once,with_context=with_context,
             best_match_only=best_match_only, preferred_data_sources=preferred_data_sources))
 
-  if(http=='get'){
-    tmp <- GET(url, query=args, callopts)
+  if (http=='get') {
+    tmp <- GET(url, query=args, ...)
     warn_for_status(tmp)
     tmp2 <- content(tmp, as = "text")
     dat <- jsonlite::fromJSON(tmp2, FALSE)$data
-  } else
-    if(http=='post'){
-      args <- args[!names(args) %in% "names"]
-      if(length(names) > 499){
-        tt <- data.frame(num = 1:length(names), names=names)
-        tt <- data.frame(ddply(tt, .(num), summarise, paste0(num,"|",names))[,2])
-        write.table(tt, file="~/gnr_names.txt", row.names=FALSE, col.names=FALSE, quote=FALSE)
-        ss <- POST(url, query=args, body=list(file = upload_file(path="~/gnr_names.txt")))
-        warn_for_status(ss)
-        ss <- content(ss)
-        bb <- "working"
-        while(bb == "working"){
-          temp <- content(GET(ss$url))
-          bb <- temp$status
-        }
-        dat <- temp$data
-      } else
-      {
-        ss <- POST(url, query=args, body=list(names = names2))
-        warn_for_status(ss)
-        dat <- content(ss)$data
-      }
-    } else
-      stop("http must be one of 'get' or 'post'")
+  } else {
+    args <- args[!names(args) %in% "names"]
+
+    nms <- split(names, ceiling(seq_along(names)/500))
+    datbits <- list()
+    for(i in seq_along(nms)){
+      tt <- data.frame(num = 1:length(nms[[i]]), names=nms[[i]])
+      tt <- data.frame(ddply(tt, .(num), summarise, paste0(num, "|", names))[,2])
+      write.table(tt, file="~/gnr_names.txt", row.names=FALSE, col.names=FALSE, quote=FALSE)
+      ss <- POST(url, query=args, body=list(file = upload_file(path="~/gnr_names.txt")), ...)
+      warn_for_status(ss)
+      ss <- content(ss, "text")
+      datbits[[i]] <- jsonlite::fromJSON(ss, FALSE)$data
+    }
+
+    dat <- do.call("c", datbits)
+  }
 
   data_ <- lapply(dat,
                   function(y)
@@ -118,7 +112,7 @@ gnr_resolve <- function(names, data_source_ids = NULL, resolve_once = FALSE,
   data_2$matched_name2 <- as.character(data_2$matched_name2)
   out <- data_2[order(data_2$submitted_name), ]
 
-  if(!is.null(preferred_data_sources)){
+  if (!is.null(preferred_data_sources)) {
     data_preferred <- lapply(dat,
       function(y)
           list(y[["supplied_name_string"]],
@@ -131,19 +125,21 @@ gnr_resolve <- function(names, data_source_ids = NULL, resolve_once = FALSE,
     data_2_preferred$matched_name2 <- as.character(data_2_preferred$matched_name2)
     out_preferred <- data_2_preferred[order(data_2_preferred$submitted_name), ]
 
-    if(stripauthority){
+    if (stripauthority){
       out_preferred <- out_preferred[ , !names(out_preferred) %in% "matched_name"]
     } else {
       out_preferred <- out_preferred[ , !names(out_preferred) %in% "matched_name2"]
     }
-  } else { out_preferred <- NULL }
+  } else {
+    out_preferred <- NULL
+  }
 
-  if(stripauthority){
+  if (stripauthority) {
     out <- out[ , !names(out) %in% "matched_name"]
   } else {
     out <- out[ , !names(out) %in% "matched_name2"]
   }
-  return( list(results=out, preferred=out_preferred) )
+  list(results=out, preferred=out_preferred)
 }
 
 check_value <- function(x){
