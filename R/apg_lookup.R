60b0fb9bf71af066b715d4243d65701542a41b65
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-05 16:00:54 -0700

    fix to man file for apg_lookup fxn #422 some code tidying while down there too, and further explanation of data used

--- R/apg_lookup.R
+++ R/apg_lookup.R
@@ -1,23 +1,30 @@
-#' Lookup in the APGII taxonomy and replace family names.
+#' Lookup in the APGIII taxonomy and replace family names
 #'
 #' @export
-#' @param taxa Taxonomic name to lookup a synonym for in APGII taxonomy.
-#' @param rank Taxonomic rank to lookup a synonym for. One of family or order.
-#' @return A APGII family or order name, or the original name if no match.
+#' @param taxa (character) Taxonomic name to lookup a synonym for in APGIII taxonomy.
+#' @param rank (character) Taxonomic rank to lookup a synonym for. One of family or order.
+#' @return A APGIII family or order name, or the original name if no match.
+#' @details Internally in this function, we use the datasets \code{\link{apg_families}}
+#' and \code{\link{apg_orders}} - see their descriptions for the data in them.
+#' The functions \code{\link{apgOrders}} \code{\link{apgFamilies}} are for scraping
+#' current content from the \url{http://www.mobot.org/MOBOT/research/APweb/} website.
+#'
+#' BEWARE: The datasets used in this function are (I think) from Version 12 of the data
+#' on \url{http://www.mobot.org/MOBOT/research/APweb/} - I'll update data asap.
 #' @examples
-#' # New name found in the APGII taxonomy
+#' # New name found
 #' apg_lookup(taxa = "Hyacinthaceae", rank = "family")
 #' apg_lookup(taxa = "Poaceae", rank = "family")
 #'
-#' # Name not found in the APGII taxonomy
+#' # Name not found
 #' apg_lookup(taxa = "Asteraceae", rank = "family")
-apg_lookup <- function(taxa, rank = "family")
-{
-  if (!rank %in% c('family', 'order'))
+apg_lookup <- function(taxa, rank = "family") {
+  if (!rank %in% c('family', 'order')) {
     stop("rank must be one of family or order")
-  if (rank == "family"){
+  }
+  if (rank == "family") {
     temp <- as.character(apg_families[apg_families$this %in% taxa, "that"])
-    if (nchar(temp)==0){
+    if (nchar(temp) == 0) {
       message("no match found...")
       out <- taxa
     } else {
@@ -25,9 +32,9 @@ apg_lookup <- function(taxa, rank = "family")
       out <- temp
     }
   } else {
-    if(rank == "order"){
+    if (rank == "order") {
       temp <- as.character(apg_orders[apg_orders$this %in% taxa, "that"])
-      if(nchar(temp)==0){
+      if (nchar(temp) == 0) {
         message("no match found...")
         out <- taxa
       } else {
