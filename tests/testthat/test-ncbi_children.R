5994c10171aa46c3229e8ddd0ae5ff99b1bfc40d
edisz <eduardszoecs@gmail.com>
2015-07-02 20:38:01 +0200

    typo

--- tests/testthat/test-ncbi_children.R
+++ tests/testthat/test-ncbi_children.R
@@ -6,12 +6,12 @@ tt <- ncbi_children(id = 4751)
 tt2 <- ncbi_children(id = 4751, out_type = 'uid')
 
 
-test_that("ncbi_childres returns correct class and result", {
+test_that("ncbi_children returns correct class and result", {
   expect_is(tt, "list")
   expect_is(tt[[1]], "data.frame")
   expect_equal(ncol(tt[[1]]), 3)
-  expect_is(tt1, "list")
-  expect_is(tt1[[1]], "vector")
+  expect_is(tt2, "list")
+  expect_is(tt2[[1]], "character")
   expect_error(ncbi_children(name = 'Ilex', id = 4751))
   expect_equal(ncbi_children(name = NA)[[1]], NA)
 })
