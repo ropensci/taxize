433a02128c82e994f24acf90eba75bb1c705bcf1
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-16 12:17:19 -0700

    fix to iucn_id test

--- tests/testthat/test-iucn_id.R
+++ tests/testthat/test-iucn_id.R
@@ -5,19 +5,21 @@ bb <- iucn_id("Panthera uncia")
 cc <- iucn_id("Lynx lynx")
 
 test_that("iucn_id returns the correct class", {
-  expect_is(aa, "numeric")
-  expect_is(bb, "numeric")
-  expect_is(cc, "numeric")
+  expect_is(aa, "list")
+  expect_is(aa[[1]], "character")
+  expect_is(bb, "list")
+  expect_is(bb[[1]], "character")
+  expect_is(cc, "list")
+  expect_is(cc[[1]], "character")
 })
 
 test_that("iucn_id returns the correct value", {
-  expect_equal(aa, 22679935)
-  expect_equal(bb, 22732)
-  expect_equal(cc, 12519)
+  expect_equal(aa[[1]], "141453")
+  expect_equal(bb[[1]], "22732")
+  expect_equal(cc[[1]], "12519")
 })
 
 test_that("iucn_id fails well", {
   expect_error(iucn_id(), "argument \"sciname\" is missing")
-  expect_warning(iucn_id("foo bar"), "foo bar - not found")
-  expect_equal(suppressWarnings(iucn_id("foo bar")), NA)
+  expect_equal(length(iucn_id("foo bar")), 0)
 })
