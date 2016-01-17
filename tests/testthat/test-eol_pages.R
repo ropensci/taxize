1a876deeca3fa98dcb31f57a3dd00eb9f80ffadd
Scott Chamberlain <myrmecocystus@gmail.com>
2016-01-17 10:28:20 -0800

    fixed eol_pages tests

--- tests/testthat/test-eol_pages.R
+++ tests/testthat/test-eol_pages.R
@@ -5,8 +5,17 @@ pageid <- suppressMessages(eol_search('Pomatomus'))$pageid[1]
 pageid2 <- suppressMessages(eol_search('Helianthus'))$pageid[1]
 
 test_that("eol_pages returns the correct value", {
-	expect_equal(suppressMessages(eol_pages(taxonconceptID=pageid))$scinames[1,1], 60986524)
-	expect_equal(suppressMessages(eol_pages(taxonconceptID=pageid2))$scinames[1,1], 32544232)
+  aa <- suppressMessages(eol_pages(taxonconceptID = pageid))
+
+  expect_is(aa, "list")
+  expect_is(aa$scinames, "data.frame")
+  expect_is(aa$syns, "character")
+
+	bb <- suppressMessages(eol_pages(taxonconceptID = pageid2))
+
+	expect_is(bb, "list")
+	expect_is(bb$scinames, "data.frame")
+	expect_is(bb$syns, "character")
 })
 
 test_that("eol_pages returns the correct class", {
