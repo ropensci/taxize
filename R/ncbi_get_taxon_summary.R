da07d00d5add8993dba5c9da86bade2489b0b9ea
Scott Chamberlain <myrmecocystus@gmail.com>
2016-06-14 17:21:35 -0700

    #541 added catch for ncbi_get_taxon_summary fxn to check for length of input ids as they will be put into the request URL and if 8000 characters or more we give a message, should help users sort it out and added notes to details in man page bumped dev version

--- R/ncbi_get_taxon_summary.R
+++ R/ncbi_get_taxon_summary.R
@@ -4,7 +4,7 @@
 #' for a set of taxonomy UIDs using eutils esummary.
 #'
 #' @export
-#' @param id (character) NCBI taxonomy uids to retrieve information for.
+#' @param id (character) NCBI taxonomy uids to retrieve information for. See Details.
 #' @param ... Curl options passed on to \code{\link[httr]{GET}}
 #' @return A \code{data.frame} with the following rows:
 #'   \describe{
@@ -13,6 +13,10 @@
 #'     \item{rank}{The taxonomic rank (e.g. 'Genus')}
 #'   }
 #' @author Zachary Foster \email{zacharyfoster1989@@Sgmail.com}
+#' @details If your input vector or list of NCBI IDs is longer than about 8000 characters
+#' (use \code{nchar(paste(ids, collapse = "+"))}), split the list up into chunks since
+#' at about that number of characters you will run into the HTTP 414 error
+#' "Request-URI Too Long".
 #' @examples \dontrun{
 #' ncbi_get_taxon_summary(c(1430660, 4751))
 #'
@@ -25,6 +29,7 @@ ncbi_get_taxon_summary <- function(id, ...) {
   if (is.null(id)) return(NULL)
   if (length(id) <= 1 && is.na(id)) return(NA)
   id <- as.character(id)
+  if (nchar(paste(id, collapse = "+")) > 8000L) message("You may want to split your ids up into chunks")
   # Make eutils esummary query ---------------------------------------------------------------------
   base_url <- "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=taxonomy"
   query <- paste0(base_url, "&id=", paste(id, collapse = "+"))
--- tests/testthat/test-ncbi_get_taxon_summary.R
+++ tests/testthat/test-ncbi_get_taxon_summary.R
@@ -15,3 +15,17 @@ test_that("ncbi_get_taxon_summary returns correct class and result", {
   expect_equal(tt3, NULL)
 })
 
+
+test_that("ncbi_get_taxon_summary behaves correctly when very large ID vector", {
+  # short UID's are okay at larger quantities
+  ids <- rep(4751, 1200)
+  expect_is(ncbi_get_taxon_summary(id = ids), "data.frame")
+
+  # but longer IDs add up of course
+  ids <- rep(1430660, 1100)
+  expect_error(suppressMessages(ncbi_get_taxon_summary(id = ids)), "Request-URI Too Long")
+  expect_message(tryCatch(ncbi_get_taxon_summary(id = ids), error = function(e) e),
+                 "You may want to split your ids up into chunks < 1000")
+
+  expect_equal(tt3, NULL)
+})
