ab731ade623fe40e52bc0daa84f112977d397d4f
Scott Chamberlain <myrmecocystus@gmail.com>
2016-06-16 11:53:48 -0700

    fixed a test, removed wikidataR pkg dep

--- tests/testthat/test-ncbi_get_taxon_summary.R
+++ tests/testthat/test-ncbi_get_taxon_summary.R
@@ -2,12 +2,13 @@
 
 context("ncbi_get_taxon_summary")
 
-
-tt <- ncbi_get_taxon_summary(c(4751))
-tt2 <- ncbi_get_taxon_summary(NA)
-tt3 <- ncbi_get_taxon_summary(id = NULL)
-
 test_that("ncbi_get_taxon_summary returns correct class and result", {
+  skip_on_cran()
+
+  tt <- ncbi_get_taxon_summary(c(4751))
+  tt2 <- ncbi_get_taxon_summary(NA)
+  tt3 <- ncbi_get_taxon_summary(id = NULL)
+
   expect_is(tt, "data.frame")
   expect_equal(ncol(tt), 3)
   expect_equal(tt[1, 3], 'kingdom')
@@ -17,6 +18,8 @@ test_that("ncbi_get_taxon_summary returns correct class and result", {
 
 
 test_that("ncbi_get_taxon_summary behaves correctly when very large ID vector", {
+  skip_on_cran()
+
   # short UID's are okay at larger quantities
   ids <- rep(4751, 1200)
   expect_is(ncbi_get_taxon_summary(id = ids), "data.frame")
@@ -25,7 +28,5 @@ test_that("ncbi_get_taxon_summary behaves correctly when very large ID vector", 
   ids <- rep(1430660, 1100)
   expect_error(suppressMessages(ncbi_get_taxon_summary(id = ids)), "Request-URI Too Long")
   expect_message(tryCatch(ncbi_get_taxon_summary(id = ids), error = function(e) e),
-                 "You may want to split your ids up into chunks < 1000")
-
-  expect_equal(tt3, NULL)
+                 "You may want to split your ids")
 })
