d6a960967cf6f51696dd43bcb300ac2dccfc6278
Scott Chamberlain <myrmecocystus@gmail.com>
2016-10-11 06:59:24 -0700

    fix a test in gbif_parse

--- tests/testthat/test-gbif_parse.R
+++ tests/testthat/test-gbif_parse.R
@@ -9,7 +9,7 @@ test_that("gbif_parse returns the correct values and dimensions classes", {
 	expect_that(as.character(tt[1,1]), matches('x Agropogon littoralis'))
 	expect_that(as.character(tt[1,2]), matches('SCIENTIFIC'))
 
-  expect_that(dim(tt), equals(c(1,9)))
+  expect_that(dim(tt), equals(c(1,11)))
 
 	expect_is(tt, "data.frame")
 	expect_is(tt$specificepithet, "character")
