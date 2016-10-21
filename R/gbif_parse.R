0953681ae4a440e6ce9f8545240bbef2fc3485e0
Scott Chamberlain <myrmecocystus@gmail.com>
2016-10-21 10:33:42 -0700

    fix #568 fix to gbif_parse - data returned must have changed, getting back some NULLs now, which I dont think used to happen now replacing any NULLs with Nas bump dev version

--- R/gbif_parse.R
+++ R/gbif_parse.R
@@ -1,7 +1,8 @@
 #' Parse taxon names using the GBIF name parser.
 #'
 #' @export
-#' @param scientificname A character vector of scientific names.
+#' @param scientificname (character) scientific names
+#' @param ... Further args passed on to \code{\link[httr]{POST}}
 #' @return A \code{data.frame} containing fields extracted from parsed
 #' taxon names. Fields returned are the union of fields extracted from
 #' all species names in \code{scientificname}.
@@ -14,15 +15,17 @@
 #'              'Secale cereale subsp. cereale', 'Secale cereale ssp. cereale',
 #'              'Vanessa atalanta (Linnaeus, 1758)'))
 #' }
-
-gbif_parse <- function(scientificname) {
+gbif_parse <- function(scientificname, ...) {
   url <- "http://api.gbif.org/v1/parser/name"
   tt <- POST(url,
             config = c(add_headers('Content-Type' =
                                    'application/json')),
-            body = jsonlite::toJSON(scientificname))
+            body = jsonlite::toJSON(scientificname), ...)
   stop_for_status(tt)
+  stopifnot(tt$headers$`content-type` == 'application/json')
   res <- jsonlite::fromJSON(con_utf8(tt), FALSE)
-  tmp <- do.call(rbind.fill, lapply(res, as.data.frame, stringsAsFactors = FALSE))
+  res <- lapply(res, function(x) Map(function(z) if (is.null(z)) NA else z, x))
+  (tmp <- data.table::setDF(
+    data.table::rbindlist(res, fill = TRUE, use.names = TRUE)))
   setNames(tmp, tolower(names(tmp)))
 }
