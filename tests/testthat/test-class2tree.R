005e772592da7498c9ea8c50e04a306bce1d1f5e
Scott Chamberlain <myrmecocystus@gmail.com>
2019-01-26 12:15:40 -0800

    add class2tree and get_uid fixgtures

--- tests/testthat/test-class2tree.R
+++ tests/testthat/test-class2tree.R
@@ -1,21 +1,19 @@
-# tests for class2tree fxn in taxize
 context("class2tree")
 
-spnames <- c('Klattia flava', 'Trollius sibiricus', 'Arachis paraguariensis',
- 'Tanacetum boreale', 'Gentiana yakushimensis','Sesamum schinzianum',
- 'Pilea verrucosa','Tibouchina striphnocalyx','Lycium dasystemum',
- 'Berkheya echinacea','Androcymbium villosum',
- 'Helianthus annuus','Madia elegans','Lupinus albicaulis',
- 'Pinus lambertiana', "Haloarcula amylolytica JCM 13557",
+spnames <- c("Klattia flava", "Trollius sibiricus", "Arachis paraguariensis",
+ "Tanacetum boreale", "Gentiana yakushimensis", "Sesamum schinzianum",
+ "Pilea verrucosa", "Tibouchina striphnocalyx", "Lycium dasystemum",
+ "Berkheya echinacea", "Androcymbium villosum",
+ "Helianthus annuus", "Madia elegans", "Lupinus albicaulis",
+ "Pinus lambertiana", "Haloarcula amylolytica JCM 13557",
  "Halomonas sp. 'Soap Lake #6'")
-
-dupnames <- c('Mus musculus', 'Escherichia coli',
-              'Haloferax denitrificans','Mus musculus')
-
+dupnames <- c("Mus musculus", "Escherichia coli",
+              "Haloferax denitrificans", "Mus musculus")
 test_that("class2tree returns the correct value and class", {
-  skip_on_cran()
+  vcr::use_cassette("class2tree_classification_call", {
+    out <- classification(spnames, db = "ncbi", verbose = FALSE)
+  })
 
-  out <- classification(spnames, db = 'ncbi', verbose = FALSE)
   out <- out[!is.na(out)]
   tr <- class2tree(out)
 
@@ -27,12 +25,14 @@ test_that("class2tree returns the correct value and class", {
   expect_is(tr$classification, "data.frame")
   expect_is(tr$distmat, "dist")
   expect_is(tr$names, "character")
-  
-  expect_equal(anyDuplicated(gsub("\\.\\d+$", "", names(tr$classification))), 0)
+  expect_equal(
+    anyDuplicated(gsub("\\.\\d+$", "", names(tr$classification))), 0)
 })
 
 test_that("class2tree will abort when input contains duplicate taxa", {
-  skip_on_cran()
-  out <- classification(dupnames, db = 'ncbi', verbose = FALSE)
-  expect_error(class2tree(out), "Input list of classifications contains duplicates")
+  vcr::use_cassette("class2tree_classification_dup_call", {
+    out <- classification(dupnames, db = "ncbi", verbose = FALSE)
+  })
+  expect_error(class2tree(out),
+    "Input list of classifications contains duplicates")
 })
--- tests/testthat/test-get_uid.R
+++ tests/testthat/test-get_uid.R
@@ -1,43 +1,38 @@
-# tests for get_uid fxn in taxize
 context("get_uid")
 
 test_that("get_uid returns the correct value", {
-  skip_on_cran()
-
-	expect_that(is.na(get_uid(c("Chironomus riparius", "aaa"), messages=FALSE)[2]),
-              is_true())
-})
-
-test_that("get_uid returns the correct class", {
-  skip_on_cran()
-
-	expect_that(get_uid(c("Chironomus riparius", "Chaetopteryx"), messages=FALSE),
-              is_a("uid"))
+  vcr::use_cassette("get_uid", {
+    x <- get_uid(c("Chironomus riparius", "aaa"), messages = FALSE)
+    z <- get_uid(c("Chironomus riparius", "Chaetopteryx"),
+      messages = FALSE)
+  })
+  expect_true(is.na(x[2]))
+  expect_is(z, "uid")
 })
 
 test_that("get_uid accepts ask-argument", {
-  skip_on_cran()
-
-  expect_that(is.na(
-    suppressWarnings(get_uid('Dugesia', ask = FALSE, messages=FALSE))),
-              is_true())
+  vcr::use_cassette("get_uid_ask_arg", {
+    x <- sw(get_uid("Dugesia", ask = FALSE, messages = FALSE))
+  })
+  expect_true(is.na(x))
 })
 
 test_that("get_uid query modifiers work", {
-  skip_on_cran()
-
-  ### w/ modifiers to the name
-  mod1 <- get_uid(sciname = "Aratinga", messages=FALSE)
-  mod2 <- get_uid(sciname = "Aratinga", modifier = "Organism", rows = 1, messages=FALSE)
+  vcr::use_cassette("get_uid_name_modifiers", {
+    mod1 <- get_uid(sciname = "Aratinga", messages = FALSE)
+    mod2 <- get_uid(sciname = "Aratinga", modifier = "Organism",
+      rows = 1, messages = FALSE)
+  })
 
   expect_is(mod1, "uid")
   expect_is(mod2, "uid")
   expect_equal(mod1[[1]], "12945")
   expect_equal(mod2[[1]], "1230190")
 
-  ### w/ rank query
-  rq1 <- get_uid(sciname = "Pinus", rank_query = "genus", messages=FALSE)
-  rq2 <- get_uid(sciname = "Pinus", rank_query = "subgenus", messages=FALSE)
+  vcr::use_cassette("get_uid_rank_modifiers", {
+    rq1 <- get_uid(sciname = "Pinus", rank_query = "genus", messages = FALSE)
+    rq2 <- get_uid(sciname = "Pinus", rank_query = "subgenus", messages = FALSE)
+  })
 
   expect_is(rq1, "uid")
   expect_is(rq2, "uid")
@@ -46,20 +41,24 @@ test_that("get_uid query modifiers work", {
 })
 
 test_that("get_uid filtering works", {
-  skip_on_cran()
-
-  ### w/ division
-  df1 <- get_uid(sciname = "Echinacea", division_filter = "eudicots", messages=FALSE)
-  df2 <- get_uid(sciname = "Echinacea", division_filter = "sea urchins", messages=FALSE)
+  vcr::use_cassette("get_uid_filtering_division", {
+    df1 <- get_uid(sciname = "Echinacea", division_filter = "eudicots",
+      messages = FALSE)
+    df2 <- get_uid(sciname = "Echinacea", division_filter = "sea urchins",
+      messages = FALSE)
+  })
 
   expect_is(df1, "uid")
   expect_is(df2, "uid")
   expect_equal(df1[[1]], "53747")
   expect_equal(df2[[1]], "7674")
 
-  ## Rank example
-  rf1 <- get_uid(sciname = "Pinus", rank_filter = "genus", rows = 2, messages=FALSE)
-  rf2 <- get_uid(sciname = "Pinus", rank_filter = "subgenus", messages=FALSE)
+  vcr::use_cassette("get_uid_filtering_rank", {
+    rf1 <- get_uid(sciname = "Pinus", rank_filter = "genus", rows = 2,
+      messages = FALSE)
+    rf2 <- get_uid(sciname = "Pinus", rank_filter = "subgenus",
+      messages = FALSE)
+  })
 
   expect_is(rf1, "uid")
   expect_is(rf2, "uid")
