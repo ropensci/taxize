29ecd538f348a27dfd0678405c1e6347a3a6554c
Scott Chamberlain <myrmecocystus@gmail.com>
2019-01-28 12:45:29 -0800

    remove iucn_summary_id tests

--- tests/testthat/test-iucn_summary.R
+++ tests/testthat/test-iucn_summary.R
@@ -7,7 +7,6 @@ test_that("iucn_summary returns the correct value", {
 
   vcr::use_cassette("iucn_summary", {
     temp <- iucn_summary(c("Panthera uncia", "Lynx lynx"))
-    temp2 <- suppressWarnings(iucn_summary_id(c(22732, 12519)))
   })
 
   expect_that(length(temp[[1]]), equals(4))
@@ -39,30 +38,4 @@ test_that("iucn_summary gives expected result for lots of names", {
   expect_equal(ff$`Chamaecrista onusta`$status, "VU")
   expect_equal(gg$`Cyornis lemprieri`$status, "NT")
   expect_equal(hh$`Frailea pumila`$status, "LC")
-})
-
-test_that("iucn_summary_id with distr_detail produces correct output", {
-  skip_on_cran()
-  if (Sys.getenv('IUCN_REDLIST_KEY') == "") {
-    skip("No IUCN api key so test not run.")
-  }
-
-  ii <- suppressWarnings(iucn_summary_id(22685566, distr_detail = TRUE))
-  expect_equal(names(ii$`22685566`$distr), c("Native", "Introduced"))
-  expect_equal(vapply(ii$`22685566`$distr, length, 0),
-               c(Native = 12, Introduced = 1))
-})
-
-test_that("iucn_summary and iucn_summary_id fail well", {
-  if (Sys.getenv('IUCN_REDLIST_KEY') == "") {
-    skip("No IUCN api key so test not run.")
-  }
-
-  vcr::use_cassette("iucn_summary_not_found_1", {
-    expect_warning(iucn_summary("Abies"), "not found")
-  })
-
-  skip_on_cran()
-  expect_warning((x <- iucn_summary_id(0)), "not found")
-  expect_equal(x[[1]]$status, NA)
 })
