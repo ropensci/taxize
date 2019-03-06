ef43218f244fe2fc712f7260c14d7819092e0586
Rekyt <matthias.grenie@ens-lyon.fr>
2019-03-06 17:48:35 +0100

    md docs for EOL function

--- R/eol_dataobjects.R
+++ R/eol_dataobjects.R
@@ -2,26 +2,26 @@
 #'
 #' @export
 #' @param id (character) The EOL data object identifier
-#' @param taxonomy (logical) Whether to return any taxonomy details from 
-#' different taxon hierarchy providers, in an array named 
+#' @param taxonomy (logical) Whether to return any taxonomy details from
+#' different taxon hierarchy providers, in an array named
 #' \code{taxonconcepts}
-#' @param language (character) provides the results in the specified language. 
-#' one of ms, de, en, es, fr, gl, it, nl, nb, oc, pt-BR, sv, tl, mk, sr, uk, ar, 
+#' @param language (character) provides the results in the specified language.
+#' one of ms, de, en, es, fr, gl, it, nl, nb, oc, pt-BR, sv, tl, mk, sr, uk, ar,
 #' zh-Hans, zh-Hant, ko
-#' @param usekey (logical) use your API key or not (\code{TRUE} or \code{FALSE})
-#' @param key (character) Your EOL API key; ; see \code{\link{taxize-authentication}} 
+#' @param usekey (logical) use your API key or not (`TRUE` or `FALSE`)
+#' @param key (character) Your EOL API key; ; see [`taxize-authentication`]
 #' for help on authentication
-#' @param ... Curl options passed on to \code{\link[crul]{HttpClient}}
+#' @param ... Curl options passed on to [`crul::HttpClient`]
 #' @details It's possible to return JSON or XML with the EOL API. However,
 #' 		this function only returns JSON for now.
-#' @return A list, optionally with a data.frame if \code{taxonomy=TRUE}
+#' @return A list, optionally with a data.frame if `taxonomy=TRUE`
 #' @examples \dontrun{
 #' eol_dataobjects(id = 7561533)
 #'
 #' # curl options
 #' eol_dataobjects(id = 7561533, verbose = TRUE)
 #' }
-eol_dataobjects <- function(id, taxonomy = TRUE, language = NULL, usekey = TRUE, 
+eol_dataobjects <- function(id, taxonomy = TRUE, language = NULL, usekey = TRUE,
   key = NULL, ...) {
 
   if (usekey) key <- getkey(key, "EOL_KEY")
