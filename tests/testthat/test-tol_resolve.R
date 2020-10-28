b7782181fc31b4adacf33d79930f8abad6ca1201
Scott Chamberlain <myrmecocystus@gmail.com>
2020-10-28 11:18:18 -0700

    fix #816 re-instate tol_resolve test after new rotl ver on cran

--- tests/testthat/test-tol_resolve.R
+++ tests/testthat/test-tol_resolve.R
@@ -49,12 +49,12 @@ test_that("do_approximate_matching works correctly", {
   expect_equal(aa$unique_name, "Nautilus")
 
   # FIXME: bring back when new rotl ver on CRAN
-  # vcr::use_cassette("tol_resolve_do_approximate_matching_false", {
-  #   expect_error(
-  #     tol_resolve("Nautilas", do_approximate_matching = FALSE),
-  #     "No matches for any of the provided taxa"
-  #   )
-  # })
+  vcr::use_cassette("tol_resolve_do_approximate_matching_false", {
+    expect_warning(
+      tol_resolve("Nautilas", do_approximate_matching = FALSE),
+      "not matched"
+    )
+  })
 })
 
 test_that("fails well", {
